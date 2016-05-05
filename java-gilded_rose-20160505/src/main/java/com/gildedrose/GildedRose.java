b6af714ccaf429fa50d02d556976331f132902e2
Paulo Clavijo Esteban <paulo@Paulos-MacBook.local>
2016-05-05 21:42:11 +0100

    Kata Gilded Rose - Refactor replace conditional with polymorphism, step 1 add clase base

--- java-gilded_rose-20160505/src/main/java/com/gildedrose/GildedRose.java
+++ java-gilded_rose-20160505/src/main/java/com/gildedrose/GildedRose.java
@@ -1,16 +1,6 @@
 package com.gildedrose;
 
 class GildedRose {
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    public static final int MAX_QUALITY = 50;
-    public static final int MIN_QUALITY = 0;
-    public static final int SELL_BY_DATE_LIMIT = 0;
-    public static final int BACKSTAGE_SELL_BY_10 = 10;
-    public static final int BACKSTAGE_SELL_BY_5 = 5;
-    public static final int QUALITY_UNIT = 1;
-    public static final int SELL_IN_UNIT = 1;
 
     Item[] items;
 
@@ -20,86 +10,9 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            applyIncreaseQualityPolicies(item);
-            applyDecreaseQualityPolicies(item);
-            applySellInPolicies(item);
+            ItemType itemType = new ItemType(item);
+            itemType.updateQuality();
         }
     }
 
-    private void applyIncreaseQualityPolicies(Item item) {
-        final boolean maxQualityNotReached = item.quality < MAX_QUALITY;
-
-        if (maxQualityNotReached) {
-            if (isBrie(item)) {
-                increaseQuality(item);
-            }
-            if (isBackstage(item)) {
-                increaseQuality(item);
-                if (item.sellIn <= BACKSTAGE_SELL_BY_5) {
-                    increaseQuality(item);
-                    increaseQuality(item);
-                } else if (item.sellIn <= BACKSTAGE_SELL_BY_10) {
-                    increaseQuality(item);
-                }
-            }
-        }
-
-    }
-
-    private void applyDecreaseQualityPolicies(Item item) {
-        final boolean minQualityNotReached = item.quality > MIN_QUALITY;
-
-        if (minQualityNotReached) {
-            if (isBrie(item)) return;
-
-            if (isBackstage(item)) {
-                if (item.sellIn <= SELL_BY_DATE_LIMIT) {
-                    item.quality = 0;
-                }
-                return;
-            }
-
-            if (isSulfuras(item)) {
-                if (item.sellIn <= SELL_BY_DATE_LIMIT) {
-                    item.quality = 0;
-                }
-                return;
-            }
-
-            if (item.sellIn <= SELL_BY_DATE_LIMIT) {
-                decreaseQuality(item);
-            }
-
-            decreaseQuality(item);
-        }
-
-    }
-
-    private void applySellInPolicies(Item item) {
-        if (isSulfuras(item)) {
-            return;
-        }
-
-        item.sellIn = item.sellIn - SELL_IN_UNIT;
-    }
-
-    private boolean isSulfuras(Item item) {
-        return item.name.equals(SULFURAS_HAND_OF_RAGNAROS);
-    }
-
-    private boolean isBrie(Item item) {
-        return item.name.equals(AGED_BRIE);
-    }
-
-    private boolean isBackstage(Item item) {
-        return item.name.equals(BACKSTAGE_PASSES);
-    }
-
-    private void increaseQuality(Item item) {
-        item.quality = item.quality + QUALITY_UNIT;
-    }
-
-    private void decreaseQuality(Item item) {
-        item.quality = item.quality - QUALITY_UNIT;
-    }
 }
