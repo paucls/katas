6a6f3ef7350b20fc95f2e5390902fb1165409fbe
Paulo Clavijo Esteban <paulo@Paulos-MacBook.local>
2016-05-22 11:29:22 +0100

    20160522-typescript-tenis_scoring - Player scores second time

--- 20160522-typescript-tenis_scoring/test/player.spec.ts
+++ 20160522-typescript-tenis_scoring/test/player.spec.ts
@@ -5,22 +5,31 @@ import {Player} from '../app/player';
 
 describe('Player', function () {
 
-    it('should start with a score of 0', () => {
-        let player = new Player();
+    let player:Player;
 
+    beforeEach(function () {
+        player = new Player();
+    });
+
+    it('should begin with a score of 0', () => {
         expect(player.getScore()).to.equal(0);
     });
 
-    describe('scores()', function() {
-    
-        it('should score a 15point the first time', function() {
-            let player = new Player();
+    describe('scores()', function () {
 
+        it('should score a 15point the first success', function () {
             player.scores();
-    
+
             expect(player.getScore()).to.equal(15);
         });
-    
+
+        it('should score a 30point the second success', function () {
+            player.scores();
+            player.scores();
+
+            expect(player.getScore()).to.equal(30);
+        });
+
     });
 
 });
