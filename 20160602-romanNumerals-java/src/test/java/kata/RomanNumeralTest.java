0ac980e4b7e46218921dc52873e1448327914d76
Paulo Clavijo Esteban <paucls@gmail.com>
2016-06-10 16:44:59 +0100

    20160602-romanNumerals-java - Refactor improving test names and removing not longer needed tests.

--- 20160602-romanNumerals-java/src/test/java/kata/RomanNumeralTest.java
+++ 20160602-romanNumerals-java/src/test/java/kata/RomanNumeralTest.java
@@ -9,12 +9,12 @@ import static org.hamcrest.CoreMatchers.*;
 public class RomanNumeralTest {
 
 	@Test
-	public void convert_1() {
+	public void convert_1_is_I() {
 		assertThat(RomanNumeral.convert(1), is("I"));
 	}
 
 	@Test
-	public void corvert_3() {
+	public void corvert_3_means_three_I() {
 		assertThat(RomanNumeral.convert(3), is("III"));
 	}
 
@@ -29,12 +29,12 @@ public class RomanNumeralTest {
 	}
 
 	@Test
-	public void convert_6() {
+	public void convert_6_means_V_and_add_I() {
 		assertThat(RomanNumeral.convert(6), is("VI"));
 	}
 
 	@Test
-	public void convert_8() {
+	public void convert_8_means_V_and_add_three_I() {
 		assertThat(RomanNumeral.convert(8), is("VIII"));
 	}
 
@@ -49,38 +49,23 @@ public class RomanNumeralTest {
 	}
 
 	@Test
-	public void convert_13() {
-		assertThat(RomanNumeral.convert(13), is("XIII"));
-	}
-
-	@Test
-	public void convert_14() {
+	public void convert_14_means_X_and_substract_I_to_V() {
 		assertThat(RomanNumeral.convert(14), is("XIV"));
 	}
 
 	@Test
-	public void convert_15() {
-		assertThat(RomanNumeral.convert(15), is("XV"));
-	}
-
-	@Test
-	public void convert_16() {
+	public void convert_16_means_X_and_add_V_and_add_I() {
 		assertThat(RomanNumeral.convert(16), is("XVI"));
 	}
 
 	@Test
-	public void convert_19() {
+	public void convert_19_means_X_and_substract_I_to_X() {
 		assertThat(RomanNumeral.convert(19), is("XIX"));
 	}
 
 	@Test
-	public void convert_25() {
+	public void convert_25_means_two_X_and_add_V() {
 		assertThat(RomanNumeral.convert(25), is("XXV"));
-	}
-
-	@Test
-	public void convert_31() {
-		assertThat(RomanNumeral.convert(31), is("XXXI"));
 	}
 
 	@Test
@@ -88,39 +73,39 @@ public class RomanNumeralTest {
 		assertThat(RomanNumeral.convert(40), is("XL"));
 	}
 
-    @Test
-    public void convert_41_means_subtract_X_to_L_and_add_I() {
-        assertThat(RomanNumeral.convert(41), is("XLI"));
-    }
+	@Test
+	public void convert_41_means_subtract_X_to_L_and_add_I() {
+		assertThat(RomanNumeral.convert(41), is("XLI"));
+	}
 
 	@Test
 	public void convert_50_is_L() {
 		assertThat(RomanNumeral.convert(50), is("L"));
 	}
 
-    @Test
-    public void convert_60_means_L_and_add_X() {
-        assertThat(RomanNumeral.convert(60), is("LX"));
-    }
+	@Test
+	public void convert_60_means_L_and_add_X() {
+		assertThat(RomanNumeral.convert(60), is("LX"));
+	}
 
-    @Test
-    public void convert_100_is_C() {
-        assertThat(RomanNumeral.convert(100), is("C"));
-    }
+	@Test
+	public void convert_100_is_C() {
+		assertThat(RomanNumeral.convert(100), is("C"));
+	}
 
-    @Test
-    public void convert_90_means_subtract_X_to_C() {
-        assertThat(RomanNumeral.convert(90), is("XC"));
-    }
+	@Test
+	public void convert_90_means_subtract_X_to_C() {
+		assertThat(RomanNumeral.convert(90), is("XC"));
+	}
 
-    @Test
-    public void convert_101_means_C_and_add_I() {
-        assertThat(RomanNumeral.convert(101), is("CI"));
-    }
+	@Test
+	public void convert_101_means_C_and_add_I() {
+		assertThat(RomanNumeral.convert(101), is("CI"));
+	}
 
-    @Test
-    public void convert_300_means_three_C() {
-        assertThat(RomanNumeral.convert(300), is("CCC"));
-    }
+	@Test
+	public void convert_300_means_three_C() {
+		assertThat(RomanNumeral.convert(300), is("CCC"));
+	}
 
 }
