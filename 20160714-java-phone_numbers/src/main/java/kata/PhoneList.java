745a0f0b0f17945abce94132111229c089f2c5db
Paulo Clavijo Esteban <paucls@gmail.com>
2016-07-14 21:27:26 +0100

    20160714-java-phone_numbers - phone list with number prefix of other.

--- 20160714-java-phone_numbers/src/main/java/kata/PhoneList.java
+++ 20160714-java-phone_numbers/src/main/java/kata/PhoneList.java
@@ -1,13 +1,22 @@
 package kata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneList {
 
+    List<String> phones = new ArrayList<>();
+
     public boolean isConsistent() {
-        return true;
+        if (phones.size() < 2) {
+            return true;
+        }
+
+        return phones.get(0).indexOf(phones.get(1)) != 0;
     }
 
     public void add(String phoneNumber) {
-
+        phones.add(phoneNumber);
     }
 
 }
--- 20160714-java-phone_numbers/src/test/java/kata/PhoneListTest.java
+++ 20160714-java-phone_numbers/src/test/java/kata/PhoneListTest.java
@@ -3,22 +3,44 @@ package kata;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PhoneListTest {
 
+    PhoneList phoneList;
+
+    @Before
+    public void setup() {
+        phoneList = new PhoneList();
+    }
+
     @Test
     public void it_should_be_consistent_when_it_is_empty() {
-        PhoneList phoneList = new PhoneList();
-
         assertThat(phoneList.isConsistent(), is(true));
     }
 
     @Test
     public void it_should_be_consistent_when_it_has_one_phone() {
-        PhoneList phoneList = new PhoneList();
         phoneList.add("91125426");
 
         assertThat(phoneList.isConsistent(), is(true));
     }
+
+    @Test
+    public void it_should_not_be_consistent_when_a_phone_is_prefix_of_other() {
+        phoneList.add("91125426");
+        phoneList.add("911");
+
+        assertThat(phoneList.isConsistent(), is(false));
+    }
+
+    @Test
+    public void it_should_be_consistent_when_a_phone_contains_other_but_its_not_prefix() {
+        phoneList.add("91125426");
+        phoneList.add("112");
+
+        assertThat(phoneList.isConsistent(), is(true));
+    }
+
 }
