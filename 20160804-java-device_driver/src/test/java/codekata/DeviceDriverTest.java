d017d33e59a9942830e2f08435f2de68c28df364
Paulo Clavijo Esteban <paucls@gmail.com>
2016-08-04 21:32:04 +0100

    20160804-java-device_driver - Refactoring of test to use mockito framework

--- 20160804-java-device_driver/src/test/java/codekata/DeviceDriverTest.java
+++ 20160804-java-device_driver/src/test/java/codekata/DeviceDriverTest.java
@@ -1,36 +1,28 @@
 package codekata;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
 public class DeviceDriverTest {
 
     @Test
     public void read_should_read_from_hardware() {
-        MockFlashMemoryDevice hardware = new MockFlashMemoryDevice();
-        DeviceDriver driver = new DeviceDriver(hardware);
+        // Arrange
+        FlashMemoryDevice deviceMock = Mockito.mock(FlashMemoryDevice.class);
+        DeviceDriver driver = new DeviceDriver(deviceMock);
 
+        when(deviceMock.read(anyLong())).thenReturn((byte) 0);
+
+        // Act
         byte data = driver.read(0xFF);
 
+        // Assert
         assertThat(data, is((byte) 0));
-        assertThat(hardware.readCallsCount, is(1));
+        verify(deviceMock).read(0xFF);
     }
 
-}
-
-class MockFlashMemoryDevice implements FlashMemoryDevice {
-
-    int readCallsCount = 0;
-
-    public byte read(long address) {
-        readCallsCount++;
-        return 0;
-    }
-
-    public void write(long address, byte data) {
-
-    }
 }
