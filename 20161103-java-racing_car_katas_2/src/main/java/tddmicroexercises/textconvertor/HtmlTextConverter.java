ffbe81db7133f44ef42e10410e11d9b6d8589828
Paulo Clavijo Esteban <paucls@gmail.com>
2016-11-03 21:47:54 +0000

    20161103-java-racing_car_katas_2 - Refactor to use dependency injectio to pass BufferReader in constructor. Updated unit tests to use a mock Buffer Reader.

--- 20161103-java-racing_car_katas_2/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
+++ 20161103-java-racing_car_katas_2/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
@@ -7,16 +7,21 @@ import java.io.IOException;
 public class HtmlTextConverter
 {
     private String fullFilenameWithPath;
+	private BufferedReader reader;
 
-    public HtmlTextConverter(String fullFilenameWithPath)
+    public HtmlTextConverter(String fullFilenameWithPath) throws IOException
     {
         this.fullFilenameWithPath = fullFilenameWithPath;
+		this.reader = new BufferedReader(new FileReader(fullFilenameWithPath));
     }
 
-    public String convertToHtml() throws IOException{
-    
-	    BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
-	    
+	public HtmlTextConverter(String fullFilenameWithPath, BufferedReader reader) {
+        this.fullFilenameWithPath = fullFilenameWithPath;
+		this.reader = reader;
+	}
+
+	public String convertToHtml() throws IOException{
+
 	    String line = reader.readLine();
 	    String html = "";
 	    while (line != null)
--- 20161103-java-racing_car_katas_2/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ 20161103-java-racing_car_katas_2/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -1,25 +1,41 @@
 package tddmicroexercises.textconvertor;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
 public class HtmlTextConverterTest {
 
-    @Test
-    public void converter_is_constructed_for_a_file_path() {
-        HtmlTextConverter converter = new HtmlTextConverter("file1.txt");
+    BufferedReader bufferReaderMock;
 
-        assertEquals("file1.txt", converter.getFilename());
+    @Before
+    public void setup() {
+        bufferReaderMock = Mockito.mock(BufferedReader.class);
+    }
+
+    @Test
+    public void converter_is_constructed_for_a_file_path() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverter("file.txt", bufferReaderMock);
+
+        assertEquals("file.txt", converter.getFilename());
     }
 
     @Test
     public void converter_adds_br_element_at_the_end_of_each_line() throws IOException {
-        HtmlTextConverter converter = new HtmlTextConverter("./file1.txt");
+        HtmlTextConverter converter = new HtmlTextConverter("file.txt", bufferReaderMock);
+
+        when(bufferReaderMock.readLine())
+                .thenReturn("Hello")
+                .thenReturn("World")
+                .thenReturn(null);
 
         String htmlCode = converter.convertToHtml();
 
@@ -28,7 +44,11 @@ public class HtmlTextConverterTest {
 
     @Test
     public void converter_scapes_special_characters() throws IOException {
-        HtmlTextConverter converter = new HtmlTextConverter("./file2.txt");
+        HtmlTextConverter converter = new HtmlTextConverter("file.txt", bufferReaderMock);
+
+        when(bufferReaderMock.readLine())
+                .thenReturn("Hello 'World'")
+                .thenReturn(null);
 
         String htmlCode = converter.convertToHtml();
 
