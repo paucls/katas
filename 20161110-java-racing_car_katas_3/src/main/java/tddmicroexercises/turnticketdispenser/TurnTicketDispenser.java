c66aee18c5a9c61b04498a67d47b0f5e0a649730
Paulo Clavijo Esteban <paucls@gmail.com>
2016-11-10 21:33:02 +0000

    20161110-java-racing_car_katas_3 - Minor refactor

--- 20161110-java-racing_car_katas_3/src/main/java/tddmicroexercises/turnticketdispenser/TurnTicketDispenser.java
+++ 20161110-java-racing_car_katas_3/src/main/java/tddmicroexercises/turnticketdispenser/TurnTicketDispenser.java
@@ -1,18 +1,13 @@
 package tddmicroexercises.turnticketdispenser;
 
-public class TurnTicketDispenser
-{
+public class TurnTicketDispenser {
     private TurnNumberSequence turnNumberSequence;
 
     public TurnTicketDispenser(TurnNumberSequence turnNumberSequence) {
         this.turnNumberSequence = turnNumberSequence;
     }
 
-    public TurnTicket dispense()
-    {
-        int newTurnNumber = turnNumberSequence.getNextTurnNumber();
-        TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
-
-        return newTurnTicket;
+    public TurnTicket dispense() {
+        return new TurnTicket(turnNumberSequence.getNextTurnNumber());
     }
 }
