4154cc87251358cd7df89720000136e033d63acc
Paulo Clavijo Esteban <paucls@gmail.com>
2016-11-10 20:53:53 +0000

    20161110-java-racing_car_katas_3 - Cover existing functionality, two dispenser should dispense different tickets

--- 20161110-java-racing_car_katas_3/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ 20161110-java-racing_car_katas_3/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -3,6 +3,7 @@ package tddmicroexercises.turnticketdispenser;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
@@ -15,7 +16,18 @@ public class TicketDispenserTest {
         TurnTicket ticket = dispenser.getTurnTicket();
 
         assertThat(ticket, notNullValue());
-        assertThat(ticket.getTurnNumber(), is(0));
+        assertThat(ticket.getTurnNumber() >= 0, is(true));
+    }
+
+    @Test
+    public void two_dispensers_should_dispense_different_tickets() {
+        TicketDispenser dispenser1 = new TicketDispenser();
+        TicketDispenser dispenser2 = new TicketDispenser();
+
+        TurnTicket ticket1 = dispenser1.getTurnTicket();
+        TurnTicket ticket2 = dispenser2.getTurnTicket();
+
+        assertThat(ticket1.getTurnNumber(), is(not(ticket2.getTurnNumber())));
     }
 
 }
