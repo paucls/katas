dd28ffbdd4809ae0cee12bd991ea85c81d251869
Paulo Clavijo Esteban <paucls@gmail.com>
2016-12-08 20:08:05 +0000

    20161208-java-game-of-life - Initial commit for game of life kata

---
+++ 20161208-java-game-of-life/src/main/java/com/katas/gameoflife/Game.java
@@ -0,0 +1,5 @@
+package com.katas.gameoflife;
+
+public class Game {
+
+}
---
+++ 20161208-java-game-of-life/src/test/java/com/katas/gameoflife/GameTest.java
@@ -0,0 +1,11 @@
+package com.katas.gameoflife;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GameTest {
+
+}
