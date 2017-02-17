473f798c6425915552d33ea2508b4d06232429e2
Paulo Clavijo Esteban <paucls@gmail.com>
2017-02-17 16:48:38 +0000

    20170216-java-snack-machine-kata - wip

--- 20170216-java-snack-machine-kata/src/main/java/com/katas/SnackMachine.java
+++ 20170216-java-snack-machine-kata/src/main/java/com/katas/SnackMachine.java
@@ -1,5 +1,48 @@
 package com.katas;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class SnackMachine {
 
+	private double amountInTransaction = 0;
+	private HashMap<Double, Integer> coinsInTransaction = new HashMap<>();
+
+	public SnackMachine() {
+		coinsInTransaction.put(0.01, 0);
+		coinsInTransaction.put(0.02, 0);
+		coinsInTransaction.put(0.05, 0);
+		coinsInTransaction.put(0.10, 0);
+		coinsInTransaction.put(0.20, 0);
+		coinsInTransaction.put(0.50, 0);
+		coinsInTransaction.put(1.00, 0);
+		coinsInTransaction.put(2.00, 0);
+	}
+
+	public double getAmountInTransaction() {
+		Double amount = 0.0;
+
+		for (Map.Entry<Double, Integer> coin : coinsInTransaction.entrySet()) {
+			amount =+ coin.getKey() * coin.getValue();
+		}
+
+		return amount;
+	}
+
+	public void insertCoin(double value) {
+		if (coinsInTransaction.get(value) == null) {
+			return;
+		}
+
+		amountInTransaction = amountInTransaction + value;
+
+		coinsInTransaction.put(value, coinsInTransaction.get(value) + 1);
+	}
+
+	public HashMap<Double, Integer> returnMoneyBack() {
+		amountInTransaction = 0;
+
+		return coinsInTransaction;
+	}
 }
--- 20170216-java-snack-machine-kata/src/test/java/com/katas/SnackMachineTest.java
+++ 20170216-java-snack-machine-kata/src/test/java/com/katas/SnackMachineTest.java
@@ -1,10 +1,61 @@
 package com.katas;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SnackMachineTest {
 
+    SnackMachine snackMachine;
+
+    @Before
+    public void setup() {
+        snackMachine = new SnackMachine();
+    }
+
+    @Test
+    public void initialized_with_0_coins_in_transaction() {
+        assertThat(snackMachine.getAmountInTransaction(), is(0.0));
+    }
+
+    @Test
+    public void insertCoin_should_add_coin_to_coins_in_transaction() {
+        snackMachine.insertCoin(1);
+
+        assertThat(snackMachine.getAmountInTransaction(), is(1.0));
+    }
+
+    @Test
+    public void insertCoin_should_allow_different_types_of_counts() {
+        snackMachine.insertCoin(1);
+        snackMachine.insertCoin(0.20);
+
+        assertThat(snackMachine.getAmountInTransaction(), is(1.20));
+    }
+
+    @Test
+    public void insertCoin_should_not_add_invalid_coin_to_transactions() {
+        snackMachine.insertCoin(0.25);
+
+        assertThat(snackMachine.getAmountInTransaction(), is(0.0));
+    }
+
+    @Test
+    public void returnMoneyBack_returns_coins_in_transaction() {
+        snackMachine.insertCoin(1);
+        snackMachine.insertCoin(1);
+        snackMachine.insertCoin(0.20);
+
+        HashMap<Double, Integer> returnedMoney = snackMachine.returnMoneyBack();
+
+        assertThat(snackMachine.getAmountInTransaction(), is(0.0));
+
+        assertThat(returnedMoney.get(1.0), is(2));
+        assertThat(returnedMoney.get(0.20), is(1));
+    }
 }
