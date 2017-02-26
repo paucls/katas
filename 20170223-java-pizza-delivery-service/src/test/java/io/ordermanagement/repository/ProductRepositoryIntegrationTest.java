2f4716927a054ee4133903a771f055f273d00c95
Paulo Clavijo Esteban <paucls@gmail.com>
2017-02-26 17:27:49 +0000

    20170223-java-pizza-delivery-service - Testing price in product repository

--- 20170223-java-pizza-delivery-service/src/test/java/io/ordermanagement/repository/ProductRepositoryIntegrationTest.java
+++ 20170223-java-pizza-delivery-service/src/test/java/io/ordermanagement/repository/ProductRepositoryIntegrationTest.java
@@ -1,6 +1,7 @@
 package io.ordermanagement.repository;
 
 import io.ordermanagement.model.Product;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -27,12 +29,15 @@ public class ProductRepositoryIntegrationTest {
     public void testFindAll() {
         Product product = new Product();
         product.setName("Pizza Margherita");
+        product.setPrice(new BigDecimal("7.5"));
+        product.setCurrency("EURO");
         this.entityManager.persist(product);
 
         List<Product> allProducts = productRepository.findAll();
 
         assertThat(allProducts.size(), is(1));
         assertThat(allProducts.get(0).getName(), is("Pizza Margherita"));
+        assertThat(allProducts.get(0).getPrice(), CoreMatchers.is(new BigDecimal("7.5")));
     }
 
 }
