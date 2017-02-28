11e73ddb914f9ed53eb1f9192657e6ba5b420014
Paulo Clavijo Esteban <paucls@gmail.com>
2017-02-28 19:11:47 +0000

    20170223-java-pizza-delivery-service - Added a FullName value object

---
+++ 20170223-java-pizza-delivery-service/src/main/java/io/ordermanagement/model/FullName.java
@@ -0,0 +1,33 @@
+package io.ordermanagement.model;
+
+import javax.persistence.Embeddable;
+
+/**
+ * FullName Value Object
+ */
+@Embeddable
+public class FullName implements ValueObject {
+
+    private String firstName;
+    private String lastName;
+
+    private FullName() {
+    }
+
+    public FullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
+}
