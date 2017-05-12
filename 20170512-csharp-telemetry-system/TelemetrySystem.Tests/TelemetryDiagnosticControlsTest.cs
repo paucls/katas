da9cafd0a2b4304d409ecd82358c2aba7d8bfc64
Paulo Clavijo Esteban <paucls@gmail.com>
2017-05-12 21:15:43 +0100

    20170512-csharp-telemetry-system - Implemented first test

---
+++ 20170512-csharp-telemetry-system/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace TDDMicroExercises.TelemetrySystem.Tests
+{
+    [TestFixture]
+    public class TelemetryDiagnosticControlsTest
+    {
+        [Test]
+        public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
+        {
+            ITelemetryClient telemetryClientMock = MockRepository.GenerateMock<ITelemetryClient>();
+            telemetryClientMock.Stub(client => client.OnlineStatus).Return(true);
+            TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClientMock);
+
+            telemetryDiagnosticControls.CheckTransmission();
+
+            telemetryClientMock.AssertWasCalled(client => client.Send(TelemetryClient.DiagnosticMessage));
+            Assert.IsNotEmpty(telemetryDiagnosticControls.DiagnosticInfo);
+        }
+    }
+}
