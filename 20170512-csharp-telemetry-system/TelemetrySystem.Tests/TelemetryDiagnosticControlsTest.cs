8cd5d9824df3f6c4cc640a67a31d1db57cebff23
Paulo Clavijo Esteban <paucls@gmail.com>
2017-05-16 22:54:42 +0100

    20170504-csharp-tire_pressure_monitoring_system - Cover with test the client conextion retry logic

--- 20170512-csharp-telemetry-system/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
+++ 20170512-csharp-telemetry-system/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Rhino.Mocks;
+using System;
 
 namespace TDDMicroExercises.TelemetrySystem.Tests
 {
@@ -10,13 +11,26 @@ namespace TDDMicroExercises.TelemetrySystem.Tests
         public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
         {
             ITelemetryClient telemetryClientMock = MockRepository.GenerateMock<ITelemetryClient>();
+            TelemetryDiagnosticControls controls = new TelemetryDiagnosticControls(telemetryClientMock);
             telemetryClientMock.Stub(client => client.OnlineStatus).Return(true);
-            TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClientMock);
 
-            telemetryDiagnosticControls.CheckTransmission();
+            controls.CheckTransmission();
 
             telemetryClientMock.AssertWasCalled(client => client.Send(TelemetryClient.DiagnosticMessage));
-            Assert.IsNotEmpty(telemetryDiagnosticControls.DiagnosticInfo);
+            Assert.IsNotEmpty(controls.DiagnosticInfo);
+        }
+
+        [Test]
+        public void CheckTransmission_should_throw_exception_if_could_not_stablish_conexion_after_3_attempts() {
+			ITelemetryClient telemetryClientMock = MockRepository.GenerateMock<ITelemetryClient>();
+            TelemetryDiagnosticControls controls = new TelemetryDiagnosticControls(telemetryClientMock);
+            telemetryClientMock.Stub(client => client.OnlineStatus).Return(false);
+
+            Assert.Throws<Exception>(() => controls.CheckTransmission());
+
+            telemetryClientMock.AssertWasCalled(client => client.Connect("*111#")
+                                                , options => options.Repeat.Times(3));
+            telemetryClientMock.AssertWasNotCalled(client => client.Send(Arg<String>.Is.Anything));
         }
     }
 }
