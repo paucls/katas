65569808813b67b4aa1a7d9fe647138602db5928
Paulo Clavijo Esteban <paucls@gmail.com>
2017-05-30 06:12:04 +0100

    20170530-csharp-ticket-dispenser - Created project

---
+++ 20170530-csharp-ticket-dispenser/TurnTicketDispenser.Tests/TicketDispenserTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace TDDMicroExercises.TurnTicketDispenser.Tests
+{
+    [TestFixture]
+    public class TicketDispenserTest
+    {
+        [Test]
+        public void GetTurnTicket_should_return_a_new_ticket_using_unique_sequence_number() {
+            
+        }
+    }
+}
