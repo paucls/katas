3466eeaa1b0542240879d2aa500855b3b7dd5055
Paulo Clavijo Esteban <paucls@gmail.com>
2017-06-03 19:24:44 +0100

    20170603-java-calculator-pbt - Created kata

---
+++ 20170603-java-calculator-pbt/src/main/java/com/katas/Calculator.java
@@ -0,0 +1,9 @@
+package com.katas;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return -1;
+    }
+
+}
---
+++ 20170603-java-calculator-pbt/src/test/java/com/katas/CalculatorProperties.java
@@ -0,0 +1,9 @@
+package com.katas;
+
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitQuickcheck.class)
+public class CalculatorProperties {
+
+}
