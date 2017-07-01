3012c427aae4be1dc90838fac4aa8c8b8e1ec4c0
Paulo Clavijo Esteban <paucls@gmail.com>
2017-07-01 06:30:04 +0100

    20170629-ts-gilded_rose - Added missing semicolons.

--- 20170629-ts-gilded_rose/app/gilded-rose.ts
+++ 20170629-ts-gilded_rose/app/gilded-rose.ts
@@ -15,21 +15,21 @@ export class GildedRose {
         if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.quality > 0) {
                 if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                    item.quality = item.quality - 1
+                    item.quality = item.quality - 1;
                 }
             }
         } else {
             if (item.quality < 50) {
-                item.quality = item.quality + 1
+                item.quality = item.quality + 1;
                 if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
                     if (item.sellIn < 11) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1
+                            item.quality = item.quality + 1;
                         }
                     }
                     if (item.sellIn < 6) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
@@ -43,15 +43,15 @@ export class GildedRose {
                 if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
                     if (item.quality > 0) {
                         if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                            item.quality = item.quality - 1
+                            item.quality = item.quality - 1;
                         }
                     }
                 } else {
-                    item.quality = item.quality - item.quality
+                    item.quality = item.quality - item.quality;
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality = item.quality + 1;
                 }
             }
         }
