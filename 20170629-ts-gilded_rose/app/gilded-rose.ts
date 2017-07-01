6816f8ea21424c64919406a18750f154ee380aa3
Paulo Clavijo Esteban <paucls@gmail.com>
2017-07-01 06:57:50 +0100

    20170629-ts-gilded_rose - Refactor flattened a conditional.

--- 20170629-ts-gilded_rose/app/gilded-rose.ts
+++ 20170629-ts-gilded_rose/app/gilded-rose.ts
@@ -21,16 +21,16 @@ export class GildedRose {
         } else {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
-                if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+            }
+            if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                }
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
