814705fb8acd9c38dd1e112d30595bba1862bfc7
Paulo Clavijo Esteban <paucls@gmail.com>
2017-07-25 22:23:48 +0100

    20170725-typescript-wordwrap - Refactor code makin use of replace in a functional programming pointfree style.

--- 20170725-typescript-wordwrap/app/wrapper.ts
+++ 20170725-typescript-wordwrap/app/wrapper.ts
@@ -1,13 +1,16 @@
 const EMPTY_SPACE = ' ';
 const NEW_LINE = '\n';
 
+const replace = searchValue => replaceValue => s => s.replace(searchValue, replaceValue);
+const replaceSpacesByNewLines = replace(EMPTY_SPACE)(NEW_LINE);
+
 export default class Wrapper {
 
     wrap(text: string, columns: number): string {
         if (text.length < columns) {
             return text;
         }
-        return text.replace(EMPTY_SPACE, NEW_LINE);
+        return replaceSpacesByNewLines(text);
     }
 
 }
