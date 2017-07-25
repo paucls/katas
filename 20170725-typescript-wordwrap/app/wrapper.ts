065451fc2d5dd5d9e22ec1f0b84c2a49b78bef11
Paulo Clavijo Esteban <paucls@gmail.com>
2017-07-25 22:52:30 +0100

    20170725-typescript-wordwrap - Refactor substring to FP style

--- 20170725-typescript-wordwrap/app/wrapper.ts
+++ 20170725-typescript-wordwrap/app/wrapper.ts
@@ -10,15 +10,20 @@ const not = x => !x;
 const contains = searchValue => s => s.indexOf(searchValue) > -1;
 const containsSpaces = contains(EMPTY_SPACE);
 
+const chunk = chunkSize => s => s.substring(0, chunkSize);
+const splitAt = position => s => [s.substring(0, position), s.substring(position)];
+
 export default class Wrapper {
 
     wrap(text: string, columns: number): string {
+        const splitAtColumn = splitAt(columns);
+
         if (length(text) < columns) {
             return text;
         }
 
         if (not(containsSpaces(text))) {
-            return text.substring(0, columns) + NEW_LINE + text.substring(columns);
+            return splitAtColumn(text)[0] + NEW_LINE + this.wrap(splitAtColumn(text)[1], columns);
         }
 
         return replaceSpacesByNewLines(text);
