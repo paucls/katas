b22b0e9f7f641d3ca0564a6610be7c81743b2d13
Paulo Clavijo Esteban <paucls@gmail.com>
2017-07-25 22:28:13 +0100

    20170725-typescript-wordwrap - Refactor code making use of text.length in a functional programming pointfree style.

--- 20170725-typescript-wordwrap/app/wrapper.ts
+++ 20170725-typescript-wordwrap/app/wrapper.ts
@@ -3,11 +3,12 @@ const NEW_LINE = '\n';
 
 const replace = searchValue => replaceValue => s => s.replace(searchValue, replaceValue);
 const replaceSpacesByNewLines = replace(EMPTY_SPACE)(NEW_LINE);
+const length = s => s.length;
 
 export default class Wrapper {
 
     wrap(text: string, columns: number): string {
-        if (text.length < columns) {
+        if (length(text) < columns) {
             return text;
         }
         return replaceSpacesByNewLines(text);
