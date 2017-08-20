8a47237fd8b01d9ba29034d2400122702806e65f
Paulo Clavijo Esteban <paucls@gmail.com>
2017-08-20 18:45:20 +0100

    20170820-java-potter-kata - Test for 5% discount

--- 20170820-java-potter-kata/src/main/java/com/katas/Basket.java
+++ 20170820-java-potter-kata/src/main/java/com/katas/Basket.java
@@ -1,10 +1,13 @@
 package com.katas;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Basket {
 
     private static final double BOOK_PRICE = 8.0;
+    private static final double DISCOUNT_2_BOOKS = 0.05;
 
     private List<String> books;
 
@@ -13,7 +16,19 @@ class Basket {
     }
 
     double calculateTotal() {
-        return books.size() * BOOK_PRICE;
+        Set<String> differentBooks = new HashSet<>(books);
+
+        Double total = books.size() * BOOK_PRICE;
+
+        if (differentBooks.size() == 2) {
+            return applyDiscount(total, DISCOUNT_2_BOOKS);
+        }
+
+        return total;
+    }
+
+    private double applyDiscount(Double amount, Double discount) {
+        return amount - amount * discount;
     }
 
 }
--- 20170820-java-potter-kata/src/test/java/com/katas/BasketTest.java
+++ 20170820-java-potter-kata/src/test/java/com/katas/BasketTest.java
@@ -11,18 +11,25 @@ import static org.junit.Assert.assertThat;
 public class BasketTest {
 
     @Test
-    public void no_discount_for_single_book() throws Exception {
+    public void no_discount_for_single_book()  {
         Basket basket = new Basket(Collections.singletonList("first book"));
 
         assertThat(basket.calculateTotal(), is(8.00));
     }
 
     @Test
-    public void no_discount_for_multiple_copies_same_book() throws Exception {
+    public void no_discount_for_multiple_copies_same_book() {
         Basket basket = new Basket(Arrays.asList("first book", "first book", "first book"));
 
         assertThat(basket.calculateTotal(), is(24.00));
     }
 
+    @Test
+    public void apply_5p_discount_when_two_diferent_books() {
+        Basket basket = new Basket(Arrays.asList("first book", "second book"));
+
+        assertThat(basket.calculateTotal(), is(16 - 16 * 0.05));
+
+    }
 
 }
