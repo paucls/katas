5d17b81d26beb22e7ea43841ff8ca0d088517658
Paulo Clavijo Esteban <paucls@gmail.com>
2017-10-12 20:32:36 +0100

    20171011-kotlin-file-logger - Extract constant

--- 20171011-kotlin-file-logger/src/main/kotlin/com/katas/FileLogger.kt
+++ 20171011-kotlin-file-logger/src/main/kotlin/com/katas/FileLogger.kt
@@ -4,8 +4,10 @@ class FileLogger(
         private val fileWriter: FileWriter,
         private val dateProvider: DateProvider) {
 
+    private val dateFormat = "yyyyMMdd"
+
     fun log(message: String) {
-        val currentDate = dateProvider.currentDate("yyyyMMdd")
+        val currentDate = dateProvider.currentDate(dateFormat)
         val fileName = "log$currentDate.txt"
 
         if (fileWriter.exist(fileName))
