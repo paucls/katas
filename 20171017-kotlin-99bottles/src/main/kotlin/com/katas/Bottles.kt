6488f367f92c70dfbe66da46285393e33e2ae10f
Paulo Clavijo Esteban <paucls@gmail.com>
2017-10-17 20:34:27 +0100

    20171017-kotlin-99bottles - First test

---
+++ 20171017-kotlin-99bottles/src/main/kotlin/com/katas/Bottles.kt
@@ -0,0 +1,12 @@
+package com.katas
+
+class Bottles {
+
+    fun verse(number: Int): String {
+        return """
+                99 bottles of beer on the wall, 99 bottles of beer.
+                Take one down and pass it around, 98 bottles of beer on the wall.
+            """.trimIndent()
+    }
+
+}
---
+++ 20171017-kotlin-99bottles/src/test/kotlin/com/katas/BottlesSpec.kt
@@ -0,0 +1,23 @@
+package com.katas
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+object BottlesSpec : Spek({
+
+    describe("99 Bottles Song") {
+
+        val bottles = Bottles()
+
+        it("first verse") {
+            assertThat(bottles.verse(99)).isEqualTo("""
+                99 bottles of beer on the wall, 99 bottles of beer.
+                Take one down and pass it around, 98 bottles of beer on the wall.
+            """.trimIndent())
+        }
+
+    }
+
+})
