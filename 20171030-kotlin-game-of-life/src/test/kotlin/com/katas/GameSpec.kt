27d3c2fed70ceba14f6a62a071ffcc43c69bdc66
Paulo Clavijo Esteban <paucls@gmail.com>
2017-11-02 19:57:31 +0000

    20171030-kotlin-game-of-life - Refactor Game tests to describe only the responsabilities of Game

--- 20171030-kotlin-game-of-life/src/test/kotlin/com/katas/GameSpec.kt
+++ 20171030-kotlin-game-of-life/src/test/kotlin/com/katas/GameSpec.kt
@@ -4,43 +4,38 @@ import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
 
 object GameSpec : Spek({
 
     describe("Game") {
 
-        on("Current Generation") {
-            it("is over when all cells are dead") {
+        describe("isGameOver()") {
+
+            it("is true when all cells are dead") {
                 val game = Game(listOf(Cell(false), Cell(false)))
 
                 assertThat(game.isGameOver()).isTrue()
             }
 
-            it("continues when next generation has alive cells") {
+            it("is false when has alive cells") {
                 val game = Game(listOf(Cell(false), Cell(true)))
 
                 assertThat(game.isGameOver()).isFalse()
             }
+
         }
 
-        on("Next Generation") {
-            it("1x1 Grid always dies") {
-                val game = Game(listOf(Cell(true)))
+        describe("next()") {
 
-                assertThat(game.next().isGameOver()).isTrue()
-            }
-
-            it("4x1 Grid where one survives") {
-                val game = Game(listOf(Cell(false), Cell(true), Cell(true), Cell(true)))
+            it("should call next generation for each cell") {
+                val game = Game(listOf(Cell(true), Cell(true), Cell(true)))
 
                 val nextGame = game.next()
-                assertThat(nextGame.isGameOver()).isFalse()
                 assertThat(nextGame.cells[0].alive).isFalse()
-                assertThat(nextGame.cells[1].alive).isFalse()
-                assertThat(nextGame.cells[2].alive).isTrue()
-                assertThat(nextGame.cells[3].alive).isFalse()
+                assertThat(nextGame.cells[1].alive).isTrue()
+                assertThat(nextGame.cells[2].alive).isFalse()
             }
+
         }
 
     }
