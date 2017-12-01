ee09a3246117010a8c12d9e74361e1fbcd3f8e6f
Paulo Clavijo Esteban <paucls@gmail.com>
2017-12-01 20:38:58 +0000

    20171201-kotlin-banking-kata - First test for empty statement

--- 20171201-kotlin-banking-kata/src/test/kotlin/com/katas/AccountSpec.kt
+++ 20171201-kotlin-banking-kata/src/test/kotlin/com/katas/AccountSpec.kt
@@ -3,16 +3,21 @@ package com.katas
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 
 object AccountSpec : Spek({
 
-    describe("Account") {
-        it("should print statement") {
-            val account = Account()
+    describe("printStatement") {
 
-            assertThat(account.printStatement()).isEqualTo("Date    Amount  Balance")
+        given("client didn't make any operation") {
+            it("prints a empty statement") {
+                val account = Account()
+
+                assertThat(account.printStatement()).isEqualTo("Date    Amount  Balance")
+            }
         }
+
     }
 
 })
