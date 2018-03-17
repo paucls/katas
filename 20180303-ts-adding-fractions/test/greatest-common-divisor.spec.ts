22ecd9dc0956397b50756b3ff5f0dc55387c8697
Paulo Clavijo Esteban <paucls@gmail.com>
2018-03-17 18:52:52 +0000

    20180303-ts-adding-fractions - example tests for gcd algorithm

--- 20180303-ts-adding-fractions/test/greatest-common-divisor.spec.ts
+++ 20180303-ts-adding-fractions/test/greatest-common-divisor.spec.ts
@@ -2,11 +2,26 @@ import { expect } from 'chai';
 
 describe('Greatest Common Divisor', () => {
 
-    it('1 and 1', () => {
+    it('a number with itself', () => {
         expect(gcd(1, 1)).to.be.equal(1);
+        expect(gcd(2, 2)).to.be.equal(2);
+        expect(gcd(-1, -1)).to.be.equal(-1);
     });
 
-    it('42 and 56', () => {
+    it('relatively prime', () => {
+        expect(gcd(7, 20)).to.be.equal(1);
+        expect(gcd(12, 13)).to.be.equal(1);
+        expect(gcd(-2, -3)).to.be.equal(-1);
+    });
+
+    it('a number multiple of the other', () => {
+        expect(gcd(2, 4)).to.be.equal(2);
+        expect(gcd(3, 9)).to.be.equal(3);
+    });
+
+    it('common factor', () => {
+        expect(gcd(6, 8)).to.be.equal(2);
+        expect(gcd(12, 14)).to.be.equal(2);
         expect(gcd(42, 56)).to.be.equal(14);
     });
 
