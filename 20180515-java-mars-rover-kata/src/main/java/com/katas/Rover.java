ed0ac2955b4ebdc7301aff3d730459ebee198972
Paulo Clavijo Esteban <paucls@gmail.com>
2018-05-15 20:47:13 +0100

    20180515-java-mars-rover-kata - Extract method

--- 20180515-java-mars-rover-kata/src/main/java/com/katas/Rover.java
+++ 20180515-java-mars-rover-kata/src/main/java/com/katas/Rover.java
@@ -10,16 +10,24 @@ class Rover {
 
     void execute(String commands) {
         if (commands.charAt(0) == 'R') {
-            if (this.direction.equals("N")) this.direction = "E";
-            else if (this.direction.equals("E")) this.direction = "S";
-            else if (this.direction.equals("S")) this.direction = "W";
-            else this.direction = "N";
+            turnRight();
         } else {
-            if (this.direction.equals("N")) this.direction = "W";
-            else if (this.direction.equals("W")) this.direction = "S";
-            else if (this.direction.equals("S")) this.direction = "E";
-            else this.direction = "N";
+            turnLeft();
         }
 
+    }
+
+    private void turnRight() {
+        if (this.direction.equals("N")) this.direction = "E";
+        else if (this.direction.equals("E")) this.direction = "S";
+        else if (this.direction.equals("S")) this.direction = "W";
+        else this.direction = "N";
+    }
+
+    private void turnLeft() {
+        if (this.direction.equals("N")) this.direction = "W";
+        else if (this.direction.equals("W")) this.direction = "S";
+        else if (this.direction.equals("S")) this.direction = "E";
+        else this.direction = "N";
     }
 }
