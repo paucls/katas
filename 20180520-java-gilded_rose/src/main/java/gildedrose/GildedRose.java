413bf32cde416b83aad6223d49b7527a2aee909e
Paulo Clavijo Esteban <paucls@gmail.com>
2018-05-20 07:15:30 +0100

    20180520-java-gilded_rose - Refactor extract method for increase quality

--- 20180520-java-gilded_rose/src/main/java/gildedrose/GildedRose.java
+++ 20180520-java-gilded_rose/src/main/java/gildedrose/GildedRose.java
@@ -18,18 +18,18 @@ class GildedRose {
                 }
             } else {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    increaseQuality(i);
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                                increaseQuality(i);
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                                increaseQuality(i);
                             }
                         }
                     }
@@ -53,10 +53,14 @@ class GildedRose {
                     }
                 } else {
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        increaseQuality(i);
                     }
                 }
             }
         }
+    }
+
+    private void increaseQuality(int amount) {
+        items[amount].quality = items[amount].quality + 1;
     }
 }
