0cfc3a18fd427c0c0f68ec6c98ad600f4e5035ab
Paulo Clavijo Esteban <paucls@gmail.com>
2018-05-21 20:06:22 +0100

    20180520-java-gilded_rose - Refactor merge if and else

--- 20180520-java-gilded_rose/src/main/java/gildedrose/GildedRose.java
+++ 20180520-java-gilded_rose/src/main/java/gildedrose/GildedRose.java
@@ -35,15 +35,13 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (items[i].name.equals("Aged Brie")) {
                     increaseQuality(i);
+                } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    dropQuality(i);
                 } else {
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        dropQuality(i);
-                    } else {
-                        if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            continue;
-                        }
-                        decreaseQuality(i);
+                    if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                        continue;
                     }
+                    decreaseQuality(i);
                 }
             }
         }
