bf0c38dd00c6b91b5f96a33f3a989524ead43b7d
Paulo Clavijo Esteban <paucls@gmail.com>
2018-05-21 19:57:37 +0100

    20180520-java-gilded_rose - Refactor moving quality lower limit conditional inside decreaseQuality method

--- 20180520-java-gilded_rose/src/main/java/gildedrose/GildedRose.java
+++ 20180520-java-gilded_rose/src/main/java/gildedrose/GildedRose.java
@@ -11,10 +11,8 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        decreaseQuality(i);
-                    }
+                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    decreaseQuality(i);
                 }
             } else {
                 increaseQuality(i);
@@ -37,10 +35,8 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                decreaseQuality(i);
-                            }
+                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            decreaseQuality(i);
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
@@ -59,6 +55,8 @@ class GildedRose {
     }
 
     private void decreaseQuality(int i) {
-        items[i].quality = items[i].quality - 1;
+        if (items[i].quality > 0) {
+            items[i].quality = items[i].quality - 1;
+        }
     }
 }
