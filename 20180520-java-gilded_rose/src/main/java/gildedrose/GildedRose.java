5d4125344c0f93fbc36499600f1622696fe939a1
Paulo Clavijo Esteban <paucls@gmail.com>
2018-05-21 20:14:21 +0100

    20180520-java-gilded_rose - Refactor extracted item names as constants

--- 20180520-java-gilded_rose/src/main/java/gildedrose/GildedRose.java
+++ 20180520-java-gilded_rose/src/main/java/gildedrose/GildedRose.java
@@ -1,23 +1,27 @@
 package gildedrose;
 
 class GildedRose {
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     Item[] items;
 
-    public GildedRose(Item[] items) {
+    GildedRose(Item[] items) {
         this.items = items;
     }
 
-    public void updateQuality() {
+    void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(AGED_BRIE)
+                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
+                if (!items[i].name.equals(SULFURAS)) {
                     decreaseQuality(i);
                 }
             } else {
                 increaseQuality(i);
 
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].name.equals(BACKSTAGE_PASSES)) {
                     if (items[i].sellIn < 11) {
                         increaseQuality(i);
                     }
@@ -28,16 +32,16 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(SULFURAS)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (items[i].name.equals("Aged Brie")) {
+                if (items[i].name.equals(AGED_BRIE)) {
                     increaseQuality(i);
-                } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                } else if (items[i].name.equals(BACKSTAGE_PASSES)) {
                     dropQuality(i);
-                } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                } else if (items[i].name.equals(SULFURAS)) {
                     continue;
                 } else {
                     decreaseQuality(i);
