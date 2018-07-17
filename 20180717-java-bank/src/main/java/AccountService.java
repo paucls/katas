c5ae7e67e8a9fef5ca0650c8257c46904c00ee0e
Paulo Clavijo Esteban <paucls@gmail.com>
2018-07-17 22:10:32 +0100

    20180717-java-bank - should record deposit transaction

--- 20180717-java-bank/src/main/java/AccountService.java
+++ 20180717-java-bank/src/main/java/AccountService.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountService {
     private final Console console;
     private final Calendar calendar;
+
+    private List<String> transactions = new ArrayList<>();
 
     public AccountService(Console console, Calendar calendar) {
         this.console = console;
@@ -8,6 +13,7 @@ public class AccountService {
     }
 
     public void deposit(int amount) {
+        this.transactions.add("20/10/2017 | " + amount + " | " + amount);
     }
 
     public void withdraw(int amount) {
@@ -15,5 +21,8 @@ public class AccountService {
 
     public void printStatement() {
         console.printLine("DATE | AMOUNT | BALANCE");
+        for (String transaction : transactions) {
+            console.printLine(transaction);
+        }
     }
 }
--- 20180717-java-bank/src/test/java/AccountServiceTest.java
+++ 20180717-java-bank/src/test/java/AccountServiceTest.java
@@ -16,4 +16,16 @@ public class AccountServiceTest {
         verify(consoleMock).printLine("DATE | AMOUNT | BALANCE");
     }
 
+    @Test
+    public void deposit_should_record_transaction() {
+        Console consoleMock = mock(Console.class);
+        Calendar calendarMock = mock(Calendar.class);
+        AccountService accountService = new AccountService(consoleMock, calendarMock);
+
+        accountService.deposit(1000);
+        accountService.printStatement();
+
+        verify(consoleMock).printLine("DATE | AMOUNT | BALANCE");
+        verify(consoleMock).printLine("20/10/2017 | 1000 | 1000");
+    }
 }
