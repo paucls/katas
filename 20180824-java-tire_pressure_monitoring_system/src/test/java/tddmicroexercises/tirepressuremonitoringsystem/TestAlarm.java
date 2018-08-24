b6118cdd94451732ece1d4a3ce8051efe8d7bb26
Paulo Clavijo Esteban <paucls@gmail.com>
2018-08-24 17:50:03 +0100

    20180824-java-tire_pressure_monitoring_system - Format code and renamed current test

--- 20180824-java-tire_pressure_monitoring_system/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ 20180824-java-tire_pressure_monitoring_system/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -1,13 +1,14 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestAlarm {
 
     @Test
-    public void foo() {
+    public void should_be_off_on_initialization() {
         Alarm alarm = new Alarm();
         assertEquals(false, alarm.isAlarmOn());
     }
