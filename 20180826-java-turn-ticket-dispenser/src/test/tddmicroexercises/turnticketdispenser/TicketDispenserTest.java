bcf56b1047cca4d84abd251c688665c0a3874c90
Paulo Clavijo Esteban <paucls@gmail.com>
2018-08-26 21:56:17 +0100

    20180826-java-turn-ticket-dispense - Should not issue same ticket twice. Ignoring previous test until I start refactoring prod code to make it testable.

--- 20180826-java-turn-ticket-dispenser/src/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ 20180826-java-turn-ticket-dispenser/src/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -1,5 +1,6 @@
 package tddmicroexercises.turnticketdispenser;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -7,11 +8,23 @@ import static org.junit.Assert.assertThat;
 
 public class TicketDispenserTest {
 
+    @Ignore
     @Test
-    public void should_return_a_new_turn_ticket() {
+    public void should_issue_a_new_turn_ticket() {
         TicketDispenser ticketDispenser = new TicketDispenser();
 
         TurnTicket turnTicket = ticketDispenser.getTurnTicket();
         assertThat(turnTicket.getTurnNumber(), is(0));
+    }
+
+    @Test
+    public void should_not_issue_same_ticket_twice() {
+        TicketDispenser ticketDispenser = new TicketDispenser();
+
+        TurnTicket turnTicket = ticketDispenser.getTurnTicket();
+        assertThat(turnTicket.getTurnNumber(), is(0));
+
+        turnTicket = ticketDispenser.getTurnTicket();
+        assertThat(turnTicket.getTurnNumber(), is(1));
     }
 }
