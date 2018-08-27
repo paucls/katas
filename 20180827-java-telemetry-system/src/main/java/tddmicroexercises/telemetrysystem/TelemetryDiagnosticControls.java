ac525d84a59f3081a72ac22ff8e7648c9ca369a6
Paulo Clavijo Esteban <paucls@gmail.com>
2018-08-27 21:31:30 +0100

    20180827-java-telemetry-system - check transmission send diagnostic message and recieve a status message

--- 20180827-java-telemetry-system/src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
+++ 20180827-java-telemetry-system/src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
@@ -10,6 +10,10 @@ public class TelemetryDiagnosticControls {
         telemetryClient = new TelemetryClient();
     }
 
+    public TelemetryDiagnosticControls(TelemetryClient telemetryClient) {
+        this.telemetryClient = telemetryClient;
+    }
+
     public String getDiagnosticInfo() {
         return diagnosticInfo;
     }
--- 20180827-java-telemetry-system/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ 20180827-java-telemetry-system/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -2,10 +2,27 @@ package tddmicroexercises.telemetrysystem;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class TelemetryDiagnosticControlsTest {
 
     @Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() {
+    public void checkTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
+        TelemetryClient clientMock = mock(TelemetryClient.class);
+        TelemetryDiagnosticControls diagnosticControls = new TelemetryDiagnosticControls(clientMock);
+        String diagnosticInfo = "a diagnostic message";
+
+        when(clientMock.getOnlineStatus()).thenReturn(true);
+        when(clientMock.receive()).thenReturn(diagnosticInfo);
+
+        diagnosticControls.checkTransmission();
+
+        verify(clientMock).send(TelemetryClient.DIAGNOSTIC_MESSAGE);
+        assertThat(diagnosticControls.getDiagnosticInfo(), is(diagnosticInfo));
     }
 
 }
