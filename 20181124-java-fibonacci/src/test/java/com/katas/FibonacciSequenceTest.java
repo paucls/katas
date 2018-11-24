56160fd2b2a1077bfe290522096af16a1087f89b
Paulo Clavijo Esteban <paucls@gmail.com>
2018-11-24 16:30:42 +0000

    20181124-java-fibonacci - Remove duplication in tests

--- 20181124-java-fibonacci/src/test/java/com/katas/FibonacciSequenceTest.java
+++ 20181124-java-fibonacci/src/test/java/com/katas/FibonacciSequenceTest.java
@@ -1,28 +1,30 @@
 package com.katas;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FibonacciSequenceTest {
+    private FibonacciSequence fibonacci;
+
+    @Before
+    public void setUp() {
+        fibonacci = new FibonacciSequence();
+    }
+
     @Test
     public void first_number_is_zero() {
-        FibonacciSequence fibonacci = new FibonacciSequence();
-
         assertThat(fibonacci.getNumber(0)).isEqualTo(0);
     }
 
     @Test
     public void second_number_is_one() {
-        FibonacciSequence fibonacci = new FibonacciSequence();
-
         assertThat(fibonacci.getNumber(1)).isEqualTo(1);
     }
 
     @Test
     public void third_number_is_one() {
-        FibonacciSequence fibonacci = new FibonacciSequence();
-
         assertThat(fibonacci.getNumber(2)).isEqualTo(1);
     }
 }
