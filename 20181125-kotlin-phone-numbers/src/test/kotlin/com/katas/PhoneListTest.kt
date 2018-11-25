89189b3bb4c1a5b293f43e1ab908b71281155ac0
Paulo Clavijo Esteban <paucls@gmail.com>
2018-11-25 11:38:55 +0000

    20181125-kotlin-phone-numbers - Refactor tests

--- 20181125-kotlin-phone-numbers/src/test/kotlin/com/katas/PhoneListTest.kt
+++ 20181125-kotlin-phone-numbers/src/test/kotlin/com/katas/PhoneListTest.kt
@@ -9,26 +9,20 @@ class PhoneListTest {
     fun `should be consistent when is empty`() {
         val phoneList = PhoneList()
 
-        val isConsistent = phoneList.isConsistent()
-
-        assertThat(isConsistent).isTrue()
+        assertThat(phoneList.isConsistent()).isTrue()
     }
 
     @Test
     fun `should be consistent when has a single phone number`() {
         val phoneList = PhoneList.of("91125426")
 
-        val isConsistent = phoneList.isConsistent()
-
-        assertThat(isConsistent).isTrue()
+        assertThat(phoneList.isConsistent()).isTrue()
     }
 
     @Test
     fun `should not be consistent when a number if prefix of another one`() {
         val phoneList = PhoneList.of("91125426", "911")
 
-        val isConsistent = phoneList.isConsistent()
-
-        assertThat(isConsistent).isFalse()
+        assertThat(phoneList.isConsistent()).isFalse()
     }
 }
