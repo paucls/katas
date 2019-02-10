175bf0273327e3217b253357a96a47221896d305
Paulo Clavijo Esteban <paucls@gmail.com>
2019-02-10 14:56:43 +0000

    20190210-csharp-passwordvalidator

---
+++ 20190210-csharp-passwordvalidator/KataPasswordValidator.Tests/PasswordValidatorTest.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+
+namespace KataPasswordValidator.Tests
+{
+    public class PasswordValidatorTest
+    {
+        readonly PasswordValidator validator = new PasswordValidator();
+
+        [Fact]
+        public void password_must_have_more_than_8_characters()
+        {
+            Assert.True(validator.IsValid("Abcd_1234"));
+            Assert.False(validator.IsValid("Abcd_123"));
+        }
+
+        [Fact]
+        public void password_must_have_a_capital_letter()
+        {
+            Assert.True(validator.IsValid("Abcd_1234"));
+            Assert.False(validator.IsValid("abcd_1234"));
+        }
+
+        [Fact]
+        public void password_must_contain_a_lower_case_letter()
+        {
+            Assert.True(validator.IsValid("Abcd_1234"));
+            Assert.False(validator.IsValid("ABCD_1234"));
+        }
+
+        [Fact]
+        public void password_must_contain_a_number()
+        {
+            Assert.True(validator.IsValid("Abcd_1234"));
+            Assert.False(validator.IsValid("Abcd_efgh"));
+        }
+
+        [Fact]
+        public void password_must_contain_an_underscore()
+        {
+            Assert.True(validator.IsValid("Abcd_1234"));
+            Assert.False(validator.IsValid("Abcd12345"));
+        }
+    }
+}
---
+++ 20190210-csharp-passwordvalidator/KataPasswordValidator/PasswordValidator.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KataPasswordValidator
+{
+    public class PasswordValidator
+    {
+        private const int MinLength = 9;
+
+        public bool IsValid(string password)
+        {
+            return HasValidLength(password)
+                   && ContainsCapitalLetter(password)
+                   && ContainsLowercaseLetter(password)
+                   && ContainsNumber(password)
+                   && ContainsUnderscore(password);
+        }
+
+        private bool HasValidLength(string password)
+        {
+            return password.Length >= MinLength;
+        }
+
+        private bool ContainsCapitalLetter(string password)
+        {
+            return Regex.IsMatch(password, "[A-Z]");
+        }
+
+        private bool ContainsLowercaseLetter(string password)
+        {
+            return Regex.IsMatch(password, "[a-z]");
+        }
+
+        private static bool ContainsNumber(string password)
+        {
+            return Regex.IsMatch(password, "\\d");
+        }
+
+        private static bool ContainsUnderscore(string password)
+        {
+            return Regex.IsMatch(password, "_");
+        }
+    }
+}
