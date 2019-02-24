73ee67e119d9cd851c753247006303f28162e4c6
Paulo Clavijo Esteban <paucls@gmail.com>
2019-02-24 17:05:36 +0000

    20190224-ts-bowling-game - Game calculates score when getting spares. Refactoring extracting statement to function

--- 20190224-ts-bowling-game/src/BowlingGame.ts
+++ 20190224-ts-bowling-game/src/BowlingGame.ts
@@ -1,20 +1,26 @@
 const NUM_FRAMES = 10;
+const ALL_PINS = 10;
 
 export class BowlingGame {
     constructor(private rolls: number[]) {}
 
     get score() {
         let result = 0;
-        let rollIdx = 0;
 
+        let rollIdx = 0;
         for (let i = 0; i < NUM_FRAMES; i++) {
-            if (this.rolls[rollIdx] + this.rolls[rollIdx + 1] === 10)
+            if (this.isSpare(rollIdx)) {
                 result += this.rolls[rollIdx + 2];
+            }
 
             result += this.rolls[rollIdx] + this.rolls[rollIdx + 1];
             rollIdx += 2;
         }
 
         return result;
+    }
+
+    private isSpare(rollIdx: number) {
+        return this.rolls[rollIdx] + this.rolls[rollIdx + 1] === ALL_PINS;
     }
 }
