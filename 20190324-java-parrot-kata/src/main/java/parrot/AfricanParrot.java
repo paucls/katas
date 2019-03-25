9c705cda6b2e5f9cadd691e71383c3013edad358
Paulo Clavijo Esteban <paucls@gmail.com>
2019-03-25 20:36:30 +0000

    20190324-java-parrot-kata - Refactor introducing constant to remove magic number.

--- 20190324-java-parrot-kata/src/main/java/parrot/AfricanParrot.java
+++ 20190324-java-parrot-kata/src/main/java/parrot/AfricanParrot.java
@@ -2,6 +2,7 @@ package parrot;
 
 public class AfricanParrot extends Parrot {
     private static final double LOAD_FACTOR = 9.0;
+    public static final int MIN_SPEED = 0;
 
     private double numberOfCoconuts;
 
@@ -11,6 +12,6 @@ public class AfricanParrot extends Parrot {
 
     @Override
     public double getSpeed() {
-        return Math.max(0, getBaseSpeed() - LOAD_FACTOR * numberOfCoconuts);
+        return Math.max(MIN_SPEED, getBaseSpeed() - LOAD_FACTOR * numberOfCoconuts);
     }
 }
