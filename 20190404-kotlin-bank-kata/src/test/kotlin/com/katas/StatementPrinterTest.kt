6cfa2617275f3b7c027ab0e8a61adcbd1423a530
Paulo Clavijo Esteban <paucls@gmail.com>
2019-04-06 09:25:53 +0100

    20190404-kotlin-bank-kata - StatementPrinter should print a statement with one transaction test

--- 20190404-kotlin-bank-kata/src/test/kotlin/com/katas/StatementPrinterTest.kt
+++ 20190404-kotlin-bank-kata/src/test/kotlin/com/katas/StatementPrinterTest.kt
@@ -1,8 +1,10 @@
 package com.katas
 
+import com.nhaarman.mockito_kotlin.inOrder
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
+import java.time.LocalDate
 
 class StatementPrinterTest {
 
@@ -15,5 +17,19 @@ class StatementPrinterTest {
         StatementPrinter(console).print(transactions)
 
         verify(console).printLine("DATE | AMOUNT | BALANCE")
+    }
+
+    @Test
+    fun `should print a statement with one transaction`() {
+        val transactions = listOf(
+                Transaction(LocalDate.of(2014, 4, 1), 1000)
+        )
+
+        StatementPrinter(console).print(transactions)
+
+        inOrder(console) {
+            verify(console).printLine("DATE | AMOUNT | BALANCE")
+            verify(console).printLine("01/04/2014 | 1000 | 1000")
+        }
     }
 }
