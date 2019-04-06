7a609bad40e164e21b597494aea97d614899f0d7
Paulo Clavijo Esteban <paucls@gmail.com>
2019-04-06 09:34:26 +0100

    20190404-kotlin-bank-kata - StatementPrinter should print a statement with one transaction implementation

--- 20190404-kotlin-bank-kata/src/main/kotlin/com/katas/StatementPrinter.kt
+++ 20190404-kotlin-bank-kata/src/main/kotlin/com/katas/StatementPrinter.kt
@@ -1,9 +1,18 @@
 package com.katas
 
+import java.time.format.DateTimeFormatter
+
 class StatementPrinter(private val console: Console) {
 
     fun print(transactions: List<Transaction>) {
         console.printLine("DATE | AMOUNT | BALANCE")
+
+        if (!transactions.isEmpty()) {
+            val transaction = transactions[0]
+            val formattedDate = DateTimeFormatter.ofPattern("dd/MM/yyyy").format(transaction.date)
+            val line = "$formattedDate | ${transaction.amount} | ${transaction.amount}"
+            console.printLine(line)
+        }
     }
 
 }
