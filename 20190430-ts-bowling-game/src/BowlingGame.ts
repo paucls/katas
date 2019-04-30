14180bfdc8a29f01294bf9b8744d8e1c14c5f4e6
Paulo Clavijo Esteban <paucls@gmail.com>
2019-04-30 21:09:59 +0100

    20190430-ts-bowling-game - Refactor removing magic numbers

--- 20190430-ts-bowling-game/src/BowlingGame.ts
+++ 20190430-ts-bowling-game/src/BowlingGame.ts
@@ -2,12 +2,15 @@ export class Game {
     private rolls: number[] = [];
 
     get score(): number {
+        const ALL_PINS = 10;
+        const NUM_FRAMES = 10;
+
         let score = 0;
         let rollIdx = 0;
 
-        for (let i = 0; i < 10; i++) {
-            if (this.rolls[i] + this.rolls[i + 1] == 10) {
-                score += 10 + this.rolls[i + 2];
+        for (let i = 0; i < NUM_FRAMES; i++) {
+            if (this.rolls[i] + this.rolls[i + 1] == ALL_PINS) {
+                score += ALL_PINS + this.rolls[i + 2];
             } else {
                 score += this.rolls[i] + this.rolls[i + 1];
             }
