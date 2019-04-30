cecdbb471a3e29f36d94db249d66146646d6ad91
Paulo Clavijo Esteban <paucls@gmail.com>
2019-04-30 21:28:16 +0100

    20190430-ts-bowling-game - Refactoring

--- 20190430-ts-bowling-game/src/BowlingGame.ts
+++ 20190430-ts-bowling-game/src/BowlingGame.ts
@@ -1,27 +1,31 @@
+const ALL_PINS = 10;
+const NUM_FRAMES = 10;
+
 export class Game {
     private rolls: number[] = [];
 
     get score(): number {
-        const ALL_PINS = 10;
-        const NUM_FRAMES = 10;
-
         let score = 0;
-        let rollIdx = 0;
 
         for (let i = 0; i < NUM_FRAMES; i++) {
-            if (this.rolls[i] == ALL_PINS) {
+            if (this.isStrike(i)) {
                 score += ALL_PINS + this.rolls[i + 1] + this.rolls[i + 2];
-                rollIdx += 1;
-            } else if (this.rolls[i] + this.rolls[i + 1] == ALL_PINS) {
+            } else if (this.isSpare(i)) {
                 score += ALL_PINS + this.rolls[i + 2];
-                rollIdx += 2;
             } else {
                 score += this.rolls[i] + this.rolls[i + 1];
-                rollIdx += 2;
             }
         }
 
         return score;
+    }
+
+    private isStrike(firstInFrame: number) {
+        return this.rolls[firstInFrame] == ALL_PINS;
+    }
+
+    private isSpare(firstInFrame: number) {
+        return this.rolls[firstInFrame] + this.rolls[firstInFrame + 1] == ALL_PINS;
     }
 
     roll(pins: number) {
