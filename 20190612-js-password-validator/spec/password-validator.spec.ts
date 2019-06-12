6299c81cd6d84e3d91e7bee198f9865d95e06e43
Paulo Clavijo Esteban <paucls@gmail.com>
2019-06-12 11:38:51 +0100

    20190612-js-password-validator - rt should tell when password is invalid when has less than 8 characters

--- 20190612-js-password-validator/spec/password-validator.spec.ts
+++ 20190612-js-password-validator/spec/password-validator.spec.ts
@@ -5,19 +5,18 @@ class PasswordValidator {
 }
 
 describe('Password Validator', () => {
+
+    let passwordValidator;
+
+    beforeEach(() => {
+        passwordValidator = new PasswordValidator();
+    });
+
     it('should tell when a password is valid', () => {
-        const passwordValidator = new PasswordValidator();
-
-        const isValid = passwordValidator.validate('Abcd_123');
-
-        expect(isValid).toBe(true)
+        expect(passwordValidator.validate('Abcd_123')).toBe(true)
     });
 
     it('should tell password is invalid when it has less than 8 characters', () => {
-        const passwordValidator = new PasswordValidator();
-
-        const isValid = passwordValidator.validate('Abcd_12');
-
-        expect(isValid).toBe(false)
+        expect(passwordValidator.validate('Abcd_12')).toBe(false)
     });
 });
