2b2a50f246a7285ce1908efe49d153a0cfc83101
Paulo Clavijo Esteban <paucls@gmail.com>
2019-06-12 16:14:26 +0100

    20190612-csharp-fibonacci - First number in sequence is 0 (test)

---
+++ 20190612-csharp-fibonacci/CsharpKataSeed/FibonacciSequence.cs
@@ -0,0 +1,10 @@
+namespace Tdd
+{
+    public class FibonacciSequence
+    {
+        public int numberAt(int i)
+        {
+            return 123;
+        }
+    }
+}
---
+++ 20190612-csharp-fibonacci/CsharpKataSeed/FibonacciSequenceTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Tdd
+{
+    [TestFixture]
+    public class FibonacciSequenceTest
+    {
+        [Test]
+        public void first_number_in_sequence_is_0()
+        {
+            FibonacciSequence fibonacciSequence = new FibonacciSequence();
+            
+            int fibonacciNumber = fibonacciSequence.numberAt(0);
+
+            Assert.That(fibonacciNumber, Is.EqualTo(0));
+        }
+    }
+}
