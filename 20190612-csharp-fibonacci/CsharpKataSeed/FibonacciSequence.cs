2bf1cdacaea6de2b3df586ffffe91983e1a68e0d
Paulo Clavijo Esteban <paucls@gmail.com>
2019-06-12 16:30:24 +0100

    20190612-csharp-fibonacci - Sixth number in sequence is 5 (green)

--- 20190612-csharp-fibonacci/CsharpKataSeed/FibonacciSequence.cs
+++ 20190612-csharp-fibonacci/CsharpKataSeed/FibonacciSequence.cs
@@ -6,7 +6,7 @@ namespace Tdd
         {
             if (position < 2)
                 return position;
-            return position - 1;
+            return numberAt(position - 1) + numberAt(position - 2);
         }
     }
 }
