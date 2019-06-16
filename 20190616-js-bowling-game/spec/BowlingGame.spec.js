f46746ddbc534fd80932a108ff0e4637d3540931
Paulo Clavijo Esteban <paucls@gmail.com>
2019-06-16 10:21:23 +0100

    20190616-js-bowling-game - Calculates score of gutter game

--- 20190616-js-bowling-game/spec/BowlingGame.spec.js
+++ 20190616-js-bowling-game/spec/BowlingGame.spec.js
@@ -1,7 +1,19 @@
+class Game {
+    constructor(rolls) {
+
+    }
+
+    score() {
+        return 0;
+    }
+}
+
 describe('Bowling Game', () => {
 
-    it('should have score 0 when gutter game', () => {
+    it('should calculate score of gutter game', () => {
+        var game = new Game([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
+        expect(game.score()).toBe(0);
     });
 
 });
