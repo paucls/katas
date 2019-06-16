b6ce54e57701858ef63472a3c99036e19d082a0c
Paulo Clavijo Esteban <paucls@gmail.com>
2019-06-16 11:23:54 +0100

    20190616-js-bowling-game - Refactor extracting calculation logic to functions

--- 20190616-js-bowling-game/src/BowlingGame.js
+++ 20190616-js-bowling-game/src/BowlingGame.js
@@ -13,13 +13,13 @@ export class Game {
 
         for (let i = 0; i < NUM_FRAMES; i++) {
             if (this.isStrike(rollIndex)) {
-                score += ALL_PINS + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+                score += this.calculateStrikeScore(rollIndex);
                 rollIndex += 1;
             } else if (this.isSpare(rollIndex)) {
-                score += ALL_PINS + this.rolls[rollIndex + 2];
+                score += this.calculateSpareScore(rollIndex);
                 rollIndex += 2;
             } else {
-                score += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+                score += this.calculateSimpleScore(rollIndex);
                 rollIndex += 2;
             }
         }
@@ -33,5 +33,18 @@ export class Game {
 
     isSpare(rollIndex) {
         return this.rolls[rollIndex] + this.rolls[rollIndex + 1] === ALL_PINS;
+    }
+
+
+    calculateStrikeScore(rollIndex) {
+        return ALL_PINS + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+    }
+
+    calculateSpareScore(rollIndex) {
+        return ALL_PINS + this.rolls[rollIndex + 2];
+    }
+
+    calculateSimpleScore(rollIndex) {
+        return this.rolls[rollIndex] + this.rolls[rollIndex + 1];
     }
 }
