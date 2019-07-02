15129cd4f8d39e71acf2dd8da05cc57cfdac722c
Paulo Clavijo Esteban <paucls@gmail.com>
2019-07-02 22:27:43 +0100

    20190702-java-gilded-rose - Extract method to express better the intent

--- 20190702-java-gilded-rose/src/main/java/gildedrose/GildedRose.java
+++ 20190702-java-gilded-rose/src/main/java/gildedrose/GildedRose.java
@@ -32,36 +32,36 @@ class GildedRose {
 
     private void updateQualityAgedBrie(Item item) {
         if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            increaseQuality(item);
         }
 
-        item.sellIn = item.sellIn - 1;
+        decreaseQuality(item);
 
         if (item.sellIn < 0) {
             if (item.quality < 50) {
-                item.quality = item.quality + 1;
+                increaseQuality(item);
             }
         }
     }
 
     private void updateQualityBackstagePasses(Item item) {
         if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            increaseQuality(item);
 
             if (item.sellIn < 11) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    increaseQuality(item);
                 }
             }
 
             if (item.sellIn < 6) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    increaseQuality(item);
                 }
             }
         }
 
-        item.sellIn = item.sellIn - 1;
+        decreaseQuality(item);
 
         if (item.sellIn < 0) {
             item.quality = 0;
@@ -76,12 +76,20 @@ class GildedRose {
             item.quality = item.quality - 1;
         }
 
-        item.sellIn = item.sellIn - 1;
+        decreaseQuality(item);
 
         if (item.sellIn < 0) {
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
         }
+    }
+
+    private void increaseQuality(Item item) {
+        item.quality++;
+    }
+
+    private void decreaseQuality(Item item) {
+        item.sellIn--;
     }
 }
