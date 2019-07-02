62ee98ea1326f7bb43574d42920eade6bf7daa78
Paulo Clavijo Esteban <paucls@gmail.com>
2019-07-02 22:20:19 +0100

    20190702-java-gilded-rose - Extracted all the logic to calculate sulfuras quality out

--- 20190702-java-gilded-rose/src/main/java/gildedrose/GildedRose.java
+++ 20190702-java-gilded-rose/src/main/java/gildedrose/GildedRose.java
@@ -18,24 +18,17 @@ class GildedRose {
             updateQualityAgedBrie(item);
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             updateQualityBackstagePasses(item);
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            calculateQualitySulfuras(item);
         } else {
             if (item.quality > 0) {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                } else {
-                    item.quality = item.quality - 1;
-                }
+                item.quality = item.quality - 1;
             }
 
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            } else {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (item.quality > 0) {
-                    if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        return;
-                    }
                     item.quality = item.quality - 1;
                 }
             }
@@ -78,5 +71,8 @@ class GildedRose {
         if (item.sellIn < 0) {
             item.quality = 0;
         }
+    }
+
+    private void calculateQualitySulfuras(Item item) {
     }
 }
