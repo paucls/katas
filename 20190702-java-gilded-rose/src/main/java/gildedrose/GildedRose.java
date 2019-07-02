3954929fc87bcc9f294e27a7de963cc07c1a3407
Paulo Clavijo Esteban <paucls@gmail.com>
2019-07-02 22:22:26 +0100

    20190702-java-gilded-rose - Extracted method for regular item

--- 20190702-java-gilded-rose/src/main/java/gildedrose/GildedRose.java
+++ 20190702-java-gilded-rose/src/main/java/gildedrose/GildedRose.java
@@ -21,17 +21,7 @@ class GildedRose {
         } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             calculateQualitySulfuras(item);
         } else {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-
-            item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn < 0) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            }
+            updateQualityRegularItem(item);
         }
     }
 
@@ -74,5 +64,19 @@ class GildedRose {
     }
 
     private void calculateQualitySulfuras(Item item) {
+    }
+
+    private void updateQualityRegularItem(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+        }
     }
 }
