7d1edd8620a06d9128333e8103f160fcc7372478
Paulo Clavijo Esteban <paucls@gmail.com>
2019-07-07 15:09:29 +0100

    20190707-java-smelly-tictactoe - Adding constructor to Tile

--- 20190707-java-smelly-tictactoe/src/main/java/Tile.java
+++ 20190707-java-smelly-tictactoe/src/main/java/Tile.java
@@ -2,4 +2,13 @@ public class Tile {
     public int X;
     public int Y;
     public char Symbol;
+
+    public Tile() {
+    }
+
+    public Tile(int x, int y, char symbol) {
+        this.X = x;
+        this.Y = y;
+        this.Symbol = symbol;
+    }
 }
