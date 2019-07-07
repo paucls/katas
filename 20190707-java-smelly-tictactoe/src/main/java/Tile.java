fa61c93eb5c32450c2ec93542307fe7f227671e9
Paulo Clavijo Esteban <paucls@gmail.com>
2019-07-07 15:10:00 +0100

    20190707-java-smelly-tictactoe - Removed unnecessary code

--- 20190707-java-smelly-tictactoe/src/main/java/Tile.java
+++ 20190707-java-smelly-tictactoe/src/main/java/Tile.java
@@ -3,9 +3,6 @@ public class Tile {
     public int Y;
     public char Symbol;
 
-    public Tile() {
-    }
-
     public Tile(int x, int y, char symbol) {
         this.X = x;
         this.Y = y;
