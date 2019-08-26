a578585a16d0af8b721e945d1d98859b56faad1f
Paulo Clavijo Esteban <paucls@gmail.com>
2019-08-26 22:28:53 +0100

    20190826-java-dependency-breaking-katas - Rewrite Checkout test as a characterization test to add coverage.

--- 20190826-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/c/CheckoutTest.java
+++ 20190826-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/c/CheckoutTest.java
@@ -2,20 +2,22 @@ package org.codecop.dependencies.c;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 public class CheckoutTest {
 
     @Test
-    public void test3() {
+    public void should_create_receipt_applying_20percent_tax() {
         Checkout checkout = new Checkout() {
             @Override
             protected void storeReceipt(Receipt receipt) {
             }
         };
 
-        checkout.createReceipt(new Money(12));
+        Receipt receipt = checkout.createReceipt(new Money(12));
 
-        assertNotNull(checkout);
+        assertEquals(
+                "[Receipt, =======, Item 1 ... 12.00, Tax    ... 2.40, ----------------, Total  ... 14.40]",
+                receipt.format().toString());
     }
 }
