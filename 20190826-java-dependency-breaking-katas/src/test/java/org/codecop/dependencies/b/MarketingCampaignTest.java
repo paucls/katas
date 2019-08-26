e296eae7f6bc01f9fdaedb36c74ea61c9bb00d7f
Paulo Clavijo Esteban <paucls@gmail.com>
2019-08-26 16:38:08 +0100

    20190826-java-dependency-breaking-katas - Renamed first test.

--- 20190826-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/b/MarketingCampaignTest.java
+++ 20190826-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/b/MarketingCampaignTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertFalse;
 public class MarketingCampaignTest {
 
     @Test
-    public void test2() {
+    public void is_not_crazy_sale_day_when_not_a_friday() {
         MarketingCampaign campaign = new MarketingCampaign();
 
         boolean isCrazySalesDay = campaign.isCrazySalesDay();
