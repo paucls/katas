964800c41cadb1a99602bbf8f8e00cee2d4fda49
Paulo Clavijo Esteban <paucls@gmail.com>
2019-09-10 20:28:44 +0100

    20190826-java-dependency-breaking-katas - Writing first test to cover country US and standard delivery. No change in prod code yet.

--- 20190826-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/d/ShippingCostTest.java
+++ 20190826-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/d/ShippingCostTest.java
@@ -2,14 +2,17 @@ package org.codecop.dependencies.d;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ShippingCostTest {
 
     @Test
-    public void test4() {
+    public void calculate_cost_when_country_US_and_standard_delivery() {
         ShippingCost shippingCost = new ShippingCost();
-        
-        assertNotNull(shippingCost);
+
+        Money cost = shippingCost.calculate(new Country("US"), DeliveryOptions.STANDARD);
+
+        assertThat(cost, equalTo(new Money(15)));
     }
 }
