9c77eaa05ed4f69004adb8f198ad6f4c7ef3ed33
Paulo Clavijo Esteban <paucls@gmail.com>
2019-09-14 13:20:01 +0100

    20190826-java-dependency-breaking-katas - Refactor test defining a testable class explicitly instead of using an abstract class.

--- 20190826-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/e/CheckoutTest.java
+++ 20190826-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/e/CheckoutTest.java
@@ -28,23 +28,28 @@ public class CheckoutTest {
     @Test(expected = OrderCancelledException.class)
     public void should_not_subscribe_user_that_want_to_subscribe_but_do_not_accept_terms() {
         Product polkaDotSocks = new Product("Polka-dot Socks");
-        Checkout checkout = new Checkout(polkaDotSocks, emailServiceMock) {
-            @Override
-            protected UserConfirmation makeSubscribeToNewsLetterConfirmation(Product product) {
-                when(subscribeToNewsLetterConfirmationMock.isAccepted()).thenReturn(true);
-                return subscribeToNewsLetterConfirmationMock;
-            }
-
-            @Override
-            protected UserConfirmation makeTermsAndConditionsConfirmation(Product product) {
-                when(termsAndConditionsConfirmationMock.isAccepted()).thenReturn(false);
-                return termsAndConditionsConfirmationMock;
-            }
-        };
+        Checkout checkout = new TestableCheckout(polkaDotSocks, emailServiceMock);
+        when(subscribeToNewsLetterConfirmationMock.isAccepted()).thenReturn(true);
+        when(termsAndConditionsConfirmationMock.isAccepted()).thenReturn(false);
 
         checkout.confirmOrder();
 
         verify(emailServiceMock, never()).subscribeUserFor(any());
     }
 
+    private class TestableCheckout extends Checkout {
+        public TestableCheckout(Product product, EmailService emailService) {
+            super(product, emailService);
+        }
+
+        @Override
+        protected UserConfirmation makeSubscribeToNewsLetterConfirmation(Product product) {
+            return subscribeToNewsLetterConfirmationMock;
+        }
+
+        @Override
+        protected UserConfirmation makeTermsAndConditionsConfirmation(Product product) {
+            return termsAndConditionsConfirmationMock;
+        }
+    }
 }
