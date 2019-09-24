70a637e89bddd44c25e55259e66e40d4f1a90465
Paulo Clavijo Esteban <paucls@gmail.com>
2019-09-24 06:05:12 +0100

    20190923-java-dependency-breaking-katas - B) Added second test for isCrazySalesDay

--- 20190923-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/b/MarketingCampaignTest.java
+++ 20190923-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/b/MarketingCampaignTest.java
@@ -11,7 +11,7 @@ public class MarketingCampaignTest {
 
     @Test
     public void should_be_crazy_sales_day_on_tuesdays() {
-        MarketingCampaign campaign = new MarketingCampaign(){
+        MarketingCampaign campaign = new MarketingCampaign() {
             @Override
             protected DayOfWeek dayOfWeek() {
                 return DayOfWeek.TUESDAY;
@@ -21,5 +21,19 @@ public class MarketingCampaignTest {
         boolean isCrazySalesDay = campaign.isCrazySalesDay();
 
         assertTrue(isCrazySalesDay);
+    }
+
+    @Test
+    public void should_no_be_crazy_sales_day_when_not_a_tuesday() {
+        MarketingCampaign campaign = new MarketingCampaign() {
+            @Override
+            protected DayOfWeek dayOfWeek() {
+                return DayOfWeek.WEDNESDAY;
+            }
+        };
+
+        boolean isCrazySalesDay = campaign.isCrazySalesDay();
+
+        assertFalse(isCrazySalesDay);
     }
 }
