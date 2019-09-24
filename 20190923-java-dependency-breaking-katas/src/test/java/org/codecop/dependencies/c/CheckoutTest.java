850b71eae41b73f700ca88a1d3108eb4b9f562fe
Paulo Clavijo Esteban <paucls@gmail.com>
2019-09-24 06:20:51 +0100

    20190923-java-dependency-breaking-katas - C) Rewrite test assertion to be meaningful and renamed test

--- 20190923-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/c/CheckoutTest.java
+++ 20190923-java-dependency-breaking-katas/src/test/java/org/codecop/dependencies/c/CheckoutTest.java
@@ -2,20 +2,21 @@ package org.codecop.dependencies.c;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class CheckoutTest {
 
     @Test
-    public void test3() {
+    public void should_add_20_percent_vat_to_total() {
         Checkout checkout = new Checkout() {
             @Override
             protected void storeReceipt(Receipt receipt) {
             }
         };
 
-        checkout.createReceipt(new Money(12));
+        Receipt receipt = checkout.createReceipt(new Money(12));
 
-        assertNotNull(checkout);
+        assertEquals("14.4", receipt.getTotal().toString());
     }
 }
