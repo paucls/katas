91d5dd45f7626c03352951c09e54fca962bda0ec
Paulo Clavijo Esteban <paucls@gmail.com>
2019-10-06 17:08:07 +0100

    20191006-java-parrot-kata - Inline getLoadFactor on the only subclass is used the AfricanParrot.

--- 20191006-java-parrot-kata/src/main/java/parrot/AfricanParrot.java
+++ 20191006-java-parrot-kata/src/main/java/parrot/AfricanParrot.java
@@ -1,12 +1,15 @@
 package parrot;
 
 public class AfricanParrot extends Parrot {
+
+    public static final double LOAD_FACTOR = 9.0;
+
     protected AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
         super(numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
     public double getSpeed() {
-        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+        return Math.max(0, getBaseSpeed() - LOAD_FACTOR * numberOfCoconuts);
     }
 }
--- 20191006-java-parrot-kata/src/main/java/parrot/Parrot.java
+++ 20191006-java-parrot-kata/src/main/java/parrot/Parrot.java
@@ -29,10 +29,6 @@ public abstract class Parrot {
         return Math.min(24.0, voltage * getBaseSpeed());
     }
 
-    protected double getLoadFactor() {
-        return 9.0;
-    }
-
     protected double getBaseSpeed() {
         return 12.0;
     }
