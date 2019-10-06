61dbfd292ac42f2ddf3a8d45ab287a85426beaa7
Paulo Clavijo Esteban <paucls@gmail.com>
2019-10-06 16:47:06 +0100

    20191006-java-parrot-kata - Norwegian parrot overrides getSpeed

--- 20191006-java-parrot-kata/src/main/java/parrot/Parrot.java
+++ 20191006-java-parrot-kata/src/main/java/parrot/Parrot.java
@@ -2,10 +2,10 @@ package parrot;
 
 public class Parrot {
 
-    private ParrotTypeEnum type;
     protected int numberOfCoconuts;
-    private double voltage;
-    private boolean isNailed;
+    protected double voltage;
+    protected boolean isNailed;
+    private ParrotTypeEnum type;
 
     protected Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         this.type = type;
@@ -26,15 +26,10 @@ public class Parrot {
     }
 
     public double getSpeed() {
-        switch (type) {
-            case NORWEGIAN_BLUE:
-                return (isNailed) ? 0 : getBaseSpeed(voltage);
-            default:
-                throw new RuntimeException("Should be unreachable");
-        }
+        throw new RuntimeException("Should be unreachable");
     }
 
-    private double getBaseSpeed(double voltage) {
+    protected double getBaseSpeed(double voltage) {
         return Math.min(24.0, voltage * getBaseSpeed());
     }
 
