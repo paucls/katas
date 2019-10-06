4f43c788050d223b6c38587d099ceb31a02700da
Paulo Clavijo Esteban <paucls@gmail.com>
2019-10-06 16:31:07 +0100

    20191006-java-parrot-kata - Make constructor protected to allow subclassing.

--- 20191006-java-parrot-kata/src/main/java/parrot/Parrot.java
+++ 20191006-java-parrot-kata/src/main/java/parrot/Parrot.java
@@ -7,7 +7,7 @@ public class Parrot {
     private double voltage;
     private boolean isNailed;
 
-    private Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+    protected Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         this.type = type;
         this.numberOfCoconuts = numberOfCoconuts;
         this.voltage = voltage;
