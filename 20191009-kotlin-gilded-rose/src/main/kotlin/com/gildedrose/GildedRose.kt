30852bea96a9eda9303a1e4cf9aca3b2ce50bf61
Paulo Clavijo Esteban <paucls@gmail.com>
2019-10-09 18:25:02 +0100

    20191009-kotlin-gilded-rose - Split conditional

--- 20191009-kotlin-gilded-rose/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ 20191009-kotlin-gilded-rose/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,11 +7,31 @@ class GildedRose(private val items: Array<Item>) {
     }
 
     private fun updateItemQuality(item: Item) {
-        if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                } else {
-                    decreaseQuality(item)
+        if (!item.name.equals("Aged Brie")) {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    } else {
+                        decreaseQuality(item)
+                    }
+                }
+            } else {
+                if (item.quality < 50) {
+                    increaseQuality(item)
+
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                increaseQuality(item)
+                            }
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                increaseQuality(item)
+                            }
+                        }
+                    }
                 }
             }
         } else {
