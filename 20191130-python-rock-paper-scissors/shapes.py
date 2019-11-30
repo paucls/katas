a57e53c7fefab955e0b8e8a6a1ece688ab79792a
Paulo Clavijo Esteban <paucls@gmail.com>
2019-11-30 16:55:05 +0000

    20191130-python-rock-paper-scissors - paper does not beat scissors

--- 20191130-python-rock-paper-scissors/shapes.py
+++ 20191130-python-rock-paper-scissors/shapes.py
@@ -1,7 +1,11 @@
 class Paper:
-    def beats(self, another):
-        return True
+    def beats(self, shape):
+        return isinstance(shape, Rock)
 
 
 class Rock:
+    pass
+
+
+class Scissors:
     pass
--- 20191130-python-rock-paper-scissors/tests/test_rock_paper_scissors_pytest.py
+++ 20191130-python-rock-paper-scissors/tests/test_rock_paper_scissors_pytest.py
@@ -1,7 +1,11 @@
-from shapes import Paper, Rock
+from shapes import Paper, Rock, Scissors
 
 
 def test_a_paper_beats_a_rock():
     paper = Paper()
     rock = Rock()
     assert paper.beats(rock) is True
+
+
+def test_a_paper_does_not_beat_scissors():
+    assert Paper().beats(Scissors()) is False
