1f06bdb8a7dd55139fb0533a4a1afc73848be915
Paulo Clavijo Esteban <paucls@gmail.com>
2019-11-30 17:02:20 +0000

    20191130-python-rock-paper-scissors - refactor assertions

--- 20191130-python-rock-paper-scissors/tests/test_rock_paper_scissors_pytest.py
+++ 20191130-python-rock-paper-scissors/tests/test_rock_paper_scissors_pytest.py
@@ -2,16 +2,16 @@ from shapes import Paper, Rock, Scissors
 
 
 def test_a_paper_beats_a_rock():
-    assert Paper().beats(Rock()) is True
+    assert Paper().beats(Rock())
 
 
 def test_a_paper_does_not_beat_scissors():
-    assert Paper().beats(Scissors()) is False
+    assert not Paper().beats(Scissors())
 
 
 def test_a_rock_beats_scissors():
-    assert Rock().beats(Scissors()) is True
+    assert Rock().beats(Scissors())
 
 
 def test_a_rock_does_not_beat_paper():
-    assert Rock().beats(Paper()) is False
+    assert not Rock().beats(Paper())
