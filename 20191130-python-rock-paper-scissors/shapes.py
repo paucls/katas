85cf9d903a49b409e634937ab8a2a05de873b3f9
Paulo Clavijo Esteban <paucls@gmail.com>
2019-12-02 20:41:03 +0000

    20191130-python-rock-paper-scissors - refactor

--- 20191130-python-rock-paper-scissors/shapes.py
+++ 20191130-python-rock-paper-scissors/shapes.py
@@ -6,22 +6,25 @@ class Shape:
     def beats(self, shape):
         pass
 
+    def is_a(self, another_shape):
+        return isinstance(self, another_shape)
+
 
 class Paper(Shape):
     def beats(self, shape: Shape):
-        return isinstance(shape, Rock)
+        return shape.is_a(Rock)
 
 
 class Rock(Shape):
     def beats(self, shape: Shape):
-        return isinstance(shape, Scissors)
+        return shape.is_a(Scissors)
 
 
 class Scissors(Shape):
     def beats(self, shape: Shape):
-        return isinstance(shape, Paper)
+        return shape.is_a(Paper)
 
 
 class Spock(Shape):
     def beats(self, shape: Shape):
-        return isinstance(shape, Scissors) or isinstance(shape, Rock)
+        return shape.is_a(Scissors) or shape.is_a(Rock)
