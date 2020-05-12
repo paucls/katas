43bbcfcee0ac92db45edf15f806669875e891014
Paulo Clavijo Esteban <paucls@gmail.com>
2020-05-12 10:05:59 +0100

    Refactor paramterized tests

--- 20200511-python-fizzbuzz/test_fizzbuzzer.py
+++ 20200511-python-fizzbuzz/test_fizzbuzzer.py
@@ -1,29 +1,24 @@
+import pytest
+
 from fizzbuzzer import FizzBuzzer
 
 
-def test_should_return_string_1_for_number_1():
-    assert FizzBuzzer().fizz_buzz(1) == "1"
+@pytest.mark.parametrize("number, expected_string",
+                         [(1, "1"),
+                          (2, "2")])
+def test_should_return_string_representation(number, expected_string):
+    assert FizzBuzzer().fizz_buzz(number) == expected_string
 
 
-def test_should_return_string_2_for_number_2():
-    assert FizzBuzzer().fizz_buzz(2) == "2"
+@pytest.mark.parametrize("number", [3, 6])
+def test_should_return_fizz_for_numbers_multiple_of_3(number):
+    assert FizzBuzzer().fizz_buzz(number) == "Fizz"
 
 
-def test_should_return_fizz_for_number_3():
-    assert FizzBuzzer().fizz_buzz(3) == "Fizz"
+@pytest.mark.parametrize("number", [5, 10])
+def test_should_return_buzz_for_number_multiple_of_5(number):
+    assert FizzBuzzer().fizz_buzz(number) == "Buzz"
 
 
-def test_should_return_fizz_for_number_multiple_of_3():
-    assert FizzBuzzer().fizz_buzz(6) == "Fizz"
-
-
-def test_should_return_buzz_for_number_5():
-    assert FizzBuzzer().fizz_buzz(5) == "Buzz"
-
-
-def test_should_return_buzz_for_number_multiple_of_5():
-    assert FizzBuzzer().fizz_buzz(10) == "Buzz"
-
-
-def test_should_return_fizz_buzz_for_number_multiple_of_3_and_5():
+def test_should_return_fizz_buzz_for_numbers_multiple_of_3_and_5():
     assert FizzBuzzer().fizz_buzz(15) == "FizzBuzz"
