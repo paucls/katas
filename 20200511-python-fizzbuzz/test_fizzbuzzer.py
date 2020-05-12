143744574f082f7a63b82c5b55089e56b66c9067
Paulo Clavijo Esteban <paucls@gmail.com>
2020-05-12 09:17:11 +0100

    20200511-python-fizzbuzz - refactor test

--- 20200511-python-fizzbuzz/test_fizzbuzzer.py
+++ 20200511-python-fizzbuzz/test_fizzbuzzer.py
@@ -2,20 +2,16 @@ from fizzbuzzer import FizzBuzzer
 
 
 def test_should_return_string_1_for_number_1():
-    result = FizzBuzzer().fizz_buzz(1)
-    assert result == "1"
+    assert FizzBuzzer().fizz_buzz(1) == "1"
 
 
 def test_should_return_string_2_for_number_2():
-    result = FizzBuzzer().fizz_buzz(2)
-    assert result == "2"
+    assert FizzBuzzer().fizz_buzz(2) == "2"
 
 
 def test_should_return_fizz_for_number_3():
-    result = FizzBuzzer().fizz_buzz(3)
-    assert result == "Fizz"
+    assert FizzBuzzer().fizz_buzz(3) == "Fizz"
 
 
 def test_should_return_fizz_for_number_multiple_of_3():
-    result = FizzBuzzer().fizz_buzz(6)
-    assert result == "Fizz"
+    assert FizzBuzzer().fizz_buzz(6) == "Fizz"
