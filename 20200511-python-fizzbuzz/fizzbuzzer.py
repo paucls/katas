b5af86be0ab844eea72955129d17984c08e5a0f8
Paulo Clavijo Esteban <paucls@gmail.com>
2020-05-12 09:23:42 +0100

    20200511-python-fizzbuzz - refactor reordering statements

--- 20200511-python-fizzbuzz/fizzbuzzer.py
+++ 20200511-python-fizzbuzz/fizzbuzzer.py
@@ -2,8 +2,8 @@ class FizzBuzzer(object):
     def fizz_buzz(self, number):
         if number % 3 == 0 and number % 5 == 0:
             return "FizzBuzz"
-        if number % 5 == 0:
-            return "Buzz"
         if number % 3 == 0:
             return "Fizz"
+        if number % 5 == 0:
+            return "Buzz"
         return str(number)
