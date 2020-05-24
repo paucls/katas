08946e3e0212129e699e8c133cfdde39733e0b2e
Paulo Clavijo Esteban <paucls@gmail.com>
2020-05-24 20:22:08 +0100

    20200524-python-bowling-game - game with score 20

--- 20200524-python-bowling-game/test/test_bowling_game.py
+++ 20200524-python-bowling-game/test/test_bowling_game.py
@@ -1,18 +1,22 @@
+import functools
+
+
 class BowlingGame:
 
     def __init__(self, rolls):
         self._rolls = rolls
 
     def score(self):
-        return 0
+        return functools.reduce(lambda a, b: a + b, self._rolls)
 
 
 def test_should_have_score_0_for_gutter_game():
-    rolls = [0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0]
+    bowling_game = BowlingGame([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    assert bowling_game.score() == 0
 
-    score = BowlingGame(rolls).score()
 
-    assert score == 0
+def test_should_have_score_20_for_game_with_1_pin_knocked_in_each_roll():
+    bowling_game = BowlingGame([1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    assert bowling_game.score() == 20
