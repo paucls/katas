4b865220056269917dd8d9c5684ce5c470343b65
Paulo Clavijo Esteban <paucls@gmail.com>
2020-05-24 20:24:45 +0100

    20200524-python-bowling-game - new test for spares behaviour

--- 20200524-python-bowling-game/test/test_bowling_game.py
+++ 20200524-python-bowling-game/test/test_bowling_game.py
@@ -11,3 +11,10 @@ def test_should_have_score_20_for_game_with_1_pin_knocked_in_each_roll():
     bowling_game = BowlingGame([1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
     assert bowling_game.score() == 20
+
+
+def test_should_have_score_155_for_game_with_spare_in_each_frame():
+    bowling_game = BowlingGame([5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+                                5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+                                10])
+    assert bowling_game.score() == 155
