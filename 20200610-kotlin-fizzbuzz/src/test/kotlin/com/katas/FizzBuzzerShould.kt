85551c9d99f63566d72402b3d24f3e0515f9f9b0
Paulo Clavijo Esteban <paucls@gmail.com>
2020-06-12 14:47:58 +0100

    20200610-kotlin-fizzbuzz - red

--- 20200610-kotlin-fizzbuzz/src/test/kotlin/com/katas/FizzBuzzerShould.kt
+++ 20200610-kotlin-fizzbuzz/src/test/kotlin/com/katas/FizzBuzzerShould.kt
@@ -10,4 +10,9 @@ class FizzBuzzerShould {
     internal fun `output number as string`() {
         assertThat(fizzBuzzer.fizzBuzz(1)).isEqualTo("1")
     }
+
+    @Test
+    internal fun `output Fizz for multiples of 3`() {
+        assertThat(fizzBuzzer.fizzBuzz(3)).isEqualTo("Fizz")
+    }
 }
