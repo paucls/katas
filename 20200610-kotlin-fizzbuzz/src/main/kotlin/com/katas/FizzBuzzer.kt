0d8fb233a83dd65680b0bdd169c48441073be3e5
Paulo Clavijo Esteban <paucls@gmail.com>
2020-06-12 14:59:21 +0100

    20200610-kotlin-fizzbuzz - green

--- 20200610-kotlin-fizzbuzz/src/main/kotlin/com/katas/FizzBuzzer.kt
+++ 20200610-kotlin-fizzbuzz/src/main/kotlin/com/katas/FizzBuzzer.kt
@@ -2,7 +2,7 @@ package com.katas
 
 class FizzBuzzer {
     fun fizzBuzz(number: Int): String {
-        if (number == 3) return "Fizz"
+        if (number % 3 == 0) return "Fizz"
         return number.toString()
     }
 }
