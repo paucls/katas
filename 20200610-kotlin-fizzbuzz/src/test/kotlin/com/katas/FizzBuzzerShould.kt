13e8f663fac07ee2a9c8bd202f4e860a872061b6
Paulo Clavijo Esteban <paucls@gmail.com>
2020-06-12 14:57:06 +0100

    20200610-kotlin-fizzbuzz - refactor

--- 20200610-kotlin-fizzbuzz/src/test/kotlin/com/katas/FizzBuzzerShould.kt
+++ 20200610-kotlin-fizzbuzz/src/test/kotlin/com/katas/FizzBuzzerShould.kt
@@ -1,24 +1,24 @@
 package com.katas
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class FizzBuzzerShould {
     private val fizzBuzzer = FizzBuzzer()
 
-    @Test
-    internal fun `output number as string`() {
-        assertThat(fizzBuzzer.fizzBuzz(1)).isEqualTo("1")
+    @ParameterizedTest
+    @CsvSource(
+            "1,  1",
+            "4,  4"
+    )
+    fun `output number as string`(number: Int, output: String) {
+        assertThat(fizzBuzzer.fizzBuzz(number)).isEqualTo(output)
     }
 
     @Test
-    internal fun `output number 2 as string`() {
-        assertThat(fizzBuzzer.fizzBuzz(2)).isEqualTo("2")
-    }
-
-    @Test
-    internal fun `output Fizz for multiples of 3`() {
+    fun `output Fizz for multiples of 3`() {
         assertThat(fizzBuzzer.fizzBuzz(3)).isEqualTo("Fizz")
     }
 }
