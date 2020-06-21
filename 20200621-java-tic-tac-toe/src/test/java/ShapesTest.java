fcdec549672e2b1e44e6926121c723b6e2f02dff
Paulo Clavijo Esteban <paucls@gmail.com>
2020-06-21 22:08:15 +0100

    20200621-java-tic-tac-toe - refactor

--- 20200621-java-tic-tac-toe/src/test/java/ShapesTest.java
+++ 20200621-java-tic-tac-toe/src/test/java/ShapesTest.java
@@ -4,17 +4,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ShapesTest {
 
+    private final Shape rock = new Rock();
+    private final Shape scissors = new Scissors();
+
     @Test
     public void rock_beats_scissor() {
-        Shape rock = new Rock();
-        Shape scissors = new Scissors();
         assertThat(rock.beats(scissors)).isTrue();
     }
 
     @Test
     public void scissors_does_not_beat_rock() {
-        Shape rock = new Rock();
-        Shape scissors = new Scissors();
         assertThat(scissors.beats(rock)).isFalse();
     }
 }
