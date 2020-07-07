182dcf7aeee4e234420eddc22d08c4a289d664df
Paulo Clavijo Esteban <paucls@gmail.com>
2020-07-07 09:00:22 +0100

    20200707-kotlin-string-calculator - test 4, red

--- 20200707-kotlin-string-calculator/src/test/kotlin/com/katas/StringCalculatorTest.kt
+++ 20200707-kotlin-string-calculator/src/test/kotlin/com/katas/StringCalculatorTest.kt
@@ -21,4 +21,9 @@ class StringCalculatorTest {
     internal fun `should add numbers when string has multiple numbers`() {
         assertThat(calculator.add("1,2,3")).isEqualTo(6)
     }
+
+    @Test
+    internal fun `should add numbers separated by new lines`() {
+        assertThat(calculator.add("1\n2,3")).isEqualTo(6)
+    }
 }
