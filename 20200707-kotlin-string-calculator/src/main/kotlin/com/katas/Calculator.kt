f0e5ed0143dec21db8e60fbe338734b9c7254d1b
Paulo Clavijo Esteban <paucls@gmail.com>
2020-07-07 08:55:50 +0100

    20200707-kotlin-string-calculator - test 3, refactor

--- 20200707-kotlin-string-calculator/src/main/kotlin/com/katas/Calculator.kt
+++ 20200707-kotlin-string-calculator/src/main/kotlin/com/katas/Calculator.kt
@@ -1,9 +1,14 @@
 package com.katas
 
 class Calculator {
+    private val delimiter = ","
+
     fun add(numbers: String): Int {
-        if (numbers.isEmpty())
-            return 0
-        return numbers.split(",").map { it.toInt() }.sum()
+        if (numbers.isEmpty()) return 0
+
+        return numbers
+                .split(delimiter)
+                .map(String::toInt)
+                .sum()
     }
 }
