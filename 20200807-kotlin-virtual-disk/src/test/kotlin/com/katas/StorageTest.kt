e77b11a57764faae74bb2c66bfa825d7ae4b1b91
Paulo Clavijo Esteban <paucls@gmail.com>
2020-08-10 20:57:18 +0100

    20200807-kotlin-virtual-disk - refactor

--- 20200807-kotlin-virtual-disk/src/test/kotlin/com/katas/StorageTest.kt
+++ 20200807-kotlin-virtual-disk/src/test/kotlin/com/katas/StorageTest.kt
@@ -4,20 +4,19 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class StorageTest {
+
+    private val storage = Storage()
+
     @Test
-    internal fun `should have a root directory`() {
-        val storage = Storage()
+    fun `should have a root directory`() {
         assertThat(storage.rootDirectory()).isEqualTo(Directory("/"))
     }
 
     @Test
-    fun `should indicate total size used`() {
-        val storage = Storage()
+    fun `should indicate the total size used by its contents`() {
         storage.rootDirectory().add(File(name = "file1", size = 100))
         storage.rootDirectory().add(File(name = "file2", size = 50))
 
-        val totalSize: Int = storage.totalSize()
-
-        assertThat(totalSize).isEqualTo(150)
+        assertThat(storage.totalSize()).isEqualTo(150)
     }
 }
