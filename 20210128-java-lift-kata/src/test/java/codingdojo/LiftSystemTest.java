0642ad77dd7b22147a5de8ab97a7c25f255218fe
Paulo Clavijo Esteban <paucls@gmail.com>
2021-01-28 22:19:02 +0000

    20210128-java-lift-kata - Refactor test

--- 20210128-java-lift-kata/src/test/java/codingdojo/LiftSystemTest.java
+++ 20210128-java-lift-kata/src/test/java/codingdojo/LiftSystemTest.java
@@ -2,9 +2,7 @@ package codingdojo;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-
+import static java.util.List.of;
 import static org.approvaltests.Approvals.verify;
 
 public class LiftSystemTest {
@@ -12,7 +10,7 @@ public class LiftSystemTest {
     @Test
     public void a_lift_should_not_move_when_there_is_no_request_for_it() {
         Lift liftA = new Lift("A", 0);
-        LiftSystem lifts = new LiftSystem(Arrays.asList(0, 1), Collections.singletonList(liftA), Collections.emptyList());
+        LiftSystem lifts = new LiftSystem(of(0, 1), of(liftA), of());
         lifts.tick();
         verify(new LiftSystemPrinter().print(lifts));
     }
@@ -20,9 +18,7 @@ public class LiftSystemTest {
     @Test
     public void a_lift_should_move_to_requested_floor_and_open_doors_in_order_to_fulfill_request() {
         Lift liftA = new Lift("A", 0);
-        LiftSystem lifts = new LiftSystem(Arrays.asList(0, 1),
-                Collections.singletonList(liftA),
-                Collections.singletonList(new Call(1, Direction.DOWN)));
+        LiftSystem lifts = new LiftSystem(of(0, 1), of(liftA), of(new Call(1, Direction.DOWN)));
         lifts.tick();
         verify(new LiftSystemPrinter().print(lifts));
     }
