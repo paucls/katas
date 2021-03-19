e7aa8e8baec2dd81a1e3d4fe6c89704f196c15d9
Paulo Clavijo Esteban <paucls@gmail.com>
2021-03-19 21:59:11 +0000

    20210319-java-fizzbuzz - refactor

--- 20210319-java-fizzbuzz/src/main/java/com/katas/FizzBuzz.java
+++ 20210319-java-fizzbuzz/src/main/java/com/katas/FizzBuzz.java
@@ -2,21 +2,20 @@ package com.katas;
 
 public class FizzBuzz {
 
-    public static final int NUMBERS_LENGTH = 100;
+    public static final int NUMBERS_TO_PRINT = 100;
 
     public String[] numbers() {
-        String[] numbers = new String[NUMBERS_LENGTH];
+        String[] fizzBuzzedNumbers = new String[NUMBERS_TO_PRINT];
 
-        for (int i = 0; i < NUMBERS_LENGTH; i++) {
+        for (int number = 1; number <= NUMBERS_TO_PRINT; number++) {
 
-            if (i == 2) {
-                numbers[i] = "Fizz";
-                continue;
+            if (number == 3) {
+                fizzBuzzedNumbers[number - 1] = "Fizz";
+            } else {
+                fizzBuzzedNumbers[number - 1] = Integer.toString(number);
             }
-
-            numbers[i] = Integer.toString(i + 1);
         }
 
-        return numbers;
+        return fizzBuzzedNumbers;
     }
 }
