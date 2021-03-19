be13f66a042ec4e1d684c5001e49c046a3eb5de1
Paulo Clavijo Esteban <paucls@gmail.com>
2021-03-19 21:41:56 +0000

    20210319-java-fizzbuzz - refactor

--- 20210319-java-fizzbuzz/src/test/java/com/katas/FizzBuzzTest.java
+++ 20210319-java-fizzbuzz/src/test/java/com/katas/FizzBuzzTest.java
@@ -2,26 +2,19 @@ package com.katas;
 
 import org.junit.Test;
 
-import java.io.PrintStream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
+
+    private final FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
     public void should_print_100_numbers() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        String[] numbers = fizzBuzz.print();
-
-        assertThat(numbers).hasSize(100);
+        assertThat(fizzBuzz.numbers()).hasSize(100);
     }
 
     @Test
     public void should_print_1_as_first_number() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
-        String[] numbers = fizzbuzz.print();
-
-        assertThat(numbers[0]).isEqualTo("1");
+        assertThat(fizzBuzz.numbers()[0]).isEqualTo("1");
     }
 }
