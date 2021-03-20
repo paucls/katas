eb95da8529073888212240db13ef73e487410711
Paulo Clavijo Esteban <paucls@gmail.com>
2021-03-20 21:58:05 +0000

    20210320-kotlin-leapyear - atypical common year if divisible by 4 and 100

--- 20210320-kotlin-leapyear/src/main/kotlin/com/katas/LeapYear.kt
+++ 20210320-kotlin-leapyear/src/main/kotlin/com/katas/LeapYear.kt
@@ -1,5 +1,5 @@
 package com.katas
 
 fun isLeapYear(year: Int): Boolean {
-    return year % 4 == 0
+    return year % 4 == 0 && year % 100 > 0
 }
--- 20210320-kotlin-leapyear/src/test/kotlin/com/katas/LeapYearTest.kt
+++ 20210320-kotlin-leapyear/src/test/kotlin/com/katas/LeapYearTest.kt
@@ -13,4 +13,9 @@ class LeapYearTest {
     internal fun `should be common year if not divisible by 4`() {
         assertThat(isLeapYear(2001)).isFalse()
     }
+
+    @Test
+    internal fun `should be atypical common year if divisible by 4 and 100`() {
+        assertThat(isLeapYear(1900)).isFalse()
+    }
 }
