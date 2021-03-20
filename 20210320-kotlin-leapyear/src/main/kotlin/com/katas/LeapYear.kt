8f7137f62db3e84e36814a0256d16e4d1c2522ff
Paulo Clavijo Esteban <paucls@gmail.com>
2021-03-20 21:59:42 +0000

    20210320-kotlin-leapyear - refactor

--- 20210320-kotlin-leapyear/src/main/kotlin/com/katas/LeapYear.kt
+++ 20210320-kotlin-leapyear/src/main/kotlin/com/katas/LeapYear.kt
@@ -1,5 +1,9 @@
 package com.katas
 
 fun isLeapYear(year: Int): Boolean {
-    return year % 4 == 0 && year % 100 > 0
+    return isDivisibleBy4(year) && !isDivisibleBy100(year)
 }
+
+private fun isDivisibleBy100(year: Int) = year % 100 == 0
+
+private fun isDivisibleBy4(year: Int) = year % 4 == 0
