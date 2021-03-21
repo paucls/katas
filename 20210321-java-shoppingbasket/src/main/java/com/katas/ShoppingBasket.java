2ebbe219715d8616bc84572d3778718fd35dcd1b
Paulo Clavijo Esteban <paucls@gmail.com>
2021-03-21 22:24:05 +0000

    20210321-java-shoppingbasket - refactor

--- 20210321-java-shoppingbasket/src/main/java/com/katas/ShoppingBasket.java
+++ 20210321-java-shoppingbasket/src/main/java/com/katas/ShoppingBasket.java
@@ -1,15 +1,10 @@
 package com.katas;
 
-import java.util.Collections;
 import java.util.List;
 
 public class ShoppingBasket {
 
     private List<Item> items;
-
-    public ShoppingBasket() {
-        this.items = Collections.emptyList();
-    }
 
     public ShoppingBasket(List<Item> items) {
         this.items = items;
--- 20210321-java-shoppingbasket/src/test/java/com/katas/ShoppingBasketTest.java
+++ 20210321-java-shoppingbasket/src/test/java/com/katas/ShoppingBasketTest.java
@@ -12,13 +12,17 @@ public class ShoppingBasketTest {
 
     @Test
     public void should_have_a_total_of_zero_when_empty() {
-        shoppingBasket = new ShoppingBasket();
+        shoppingBasket = aShoppingBasket();
         assertThat(shoppingBasket.getTotal()).isZero();
     }
 
     @Test
     public void should_calculate_total_for_single_item() {
-        shoppingBasket = new ShoppingBasket(Arrays.asList(new Item(100, 1)));
+        shoppingBasket = aShoppingBasket(new Item(100, 1));
         assertThat(shoppingBasket.getTotal()).isEqualTo(100);
+    }
+
+    private ShoppingBasket aShoppingBasket(Item... items) {
+        return new ShoppingBasket(Arrays.asList(items));
     }
 }
