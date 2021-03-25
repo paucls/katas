ad83e43acbdbdd45cc03f9602ecd3ae5007c898b
Paulo Clavijo Esteban <paucls@gmail.com>
2021-03-25 21:08:29 +0000

    20210325-kotlin-fizzbuzz - returns string Fizz

--- 20210325-kotlin-fizzbuzz/src/main/kotlin/com/katas/FizzBuzzer.kt
+++ 20210325-kotlin-fizzbuzz/src/main/kotlin/com/katas/FizzBuzzer.kt
@@ -2,6 +2,14 @@ package com.katas
 
 class FizzBuzzer {
     fun fizzBuzz(number: Int): String {
-        return number.toString()
+        return when {
+            isFizz(number) -> "Fizz"
+            isBuzz(number) -> "Buzz"
+            else -> number.toString()
+        }
     }
+
+    private fun isFizz(number: Int) = number % 3 == 0
+
+    private fun isBuzz(number: Int) = number % 5 == 0
 }
--- 20210325-kotlin-fizzbuzz/src/test/kotlin/com/katas/FizzBuzzerTest.kt
+++ 20210325-kotlin-fizzbuzz/src/test/kotlin/com/katas/FizzBuzzerTest.kt
@@ -13,7 +13,19 @@ class FizzBuzzerTest {
     }
 
     @Test
-    internal fun `should return the string 2 for number 2`() {
+    fun `should return the string 2 for number 2`() {
         assertThat(fizzBuzzer.fizzBuzz(2)).isEqualTo("2")
+    }
+
+    @Test
+    fun `should return Fizz for numbers divisible by 3`() {
+        assertThat(fizzBuzzer.fizzBuzz(3)).isEqualTo("Fizz")
+        assertThat(fizzBuzzer.fizzBuzz(9)).isEqualTo("Fizz")
+    }
+
+    @Test
+    fun `should return Buzz for numbers divisible by 5`() {
+        assertThat(fizzBuzzer.fizzBuzz(5)).isEqualTo("Buzz")
+        assertThat(fizzBuzzer.fizzBuzz(25)).isEqualTo("Buzz")
     }
 }
