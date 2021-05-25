9be77a617157b3c32150fa588bd0493096f9791a
Paulo Clavijo Esteban <paucls@gmail.com>
2021-05-25 18:56:53 +0100

    20210519-java-fantasybattle-refactoring - Updated test

--- 20210518-java-fantasybattle-refactoring/src/test/java/codingdojo/PlayerTest.java
+++ 20210518-java-fantasybattle-refactoring/src/test/java/codingdojo/PlayerTest.java
@@ -25,15 +25,18 @@ public class PlayerTest {
     // choose this one if you are not familiar with mocks
     @Test
     void damageCalculations() {
-        BasicItem sword = new BasicItem("sword", 10, 1);
-        BasicItem zeroDamageItem = new BasicItem("empty hand", 0, 0);
-        Equipment equipment = new Equipment(sword, zeroDamageItem, zeroDamageItem, zeroDamageItem, zeroDamageItem);
+        BasicItem leftHand = new BasicItem("sword", 10, 1.0F);
+        BasicItem rightHand = new BasicItem("shield", 0, 1.4F);
+        BasicItem head = new BasicItem("helmet", 0, 1.2F);
+        BasicItem feet = new BasicItem("boots", 0, 0.1F);
+        BasicItem chest = new BasicItem("breastplate", 0, 1.4F);
+        Equipment equipment = new Equipment(leftHand, rightHand, head, feet, chest);
         Inventory inventory = new Inventory(equipment);
         Stats stats = new Stats(0);
-        SimpleEnemy target = new SimpleEnemy(new SimpleArmor(0), Collections.emptyList());
+        SimpleEnemy target = new SimpleEnemy(new SimpleArmor(5), Collections.emptyList());
 
         Damage damage = new Player(inventory, stats).calculateDamage(target);
 
-        assertEquals(10, damage.getAmount());
+        assertEquals(46, damage.getAmount());
     }
 }
