fe82b69dee6da3383665cb3797923ea2f8b008df
Paulo Clavijo Esteban <paucls@gmail.com>
2021-06-08 22:18:56 +0100

    20210608-java-birthday-greetings - moving logic to send email to the output adapter smtpEmailSender

---
+++ 20210608-java-birthday-greetings/src/main/java/birthdaygreetings/adapters/out/SmtpEmailServer.java
@@ -0,0 +1,49 @@
+package birthdaygreetings.adapters.out;
+
+import birthdaygreetings.EmailSender;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class JavaxEmailServer implements EmailSender {
+    public JavaxEmailServer(String smtpHost, int smtpPort) {
+        
+    }
+
+    @Override
+    public void sendMessage(String subject, String body, String recipient) {
+
+    }
+
+    private void sendMessage(String smtpHost, int smtpPort,
+                             String subject, String body, String recipient)
+            throws AddressException, MessagingException {
+        // Create a mail session
+        java.util.Properties props = new java.util.Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", "" + smtpPort);
+        Session session = Session.getDefaultInstance(props, null);
+
+        // Construct the message
+        Message msg = new MimeMessage(session);
+        String sender = "sender@here.com";
+        msg.setFrom(new InternetAddress(sender));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(
+                recipient));
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        // Send the message
+        sendMessage(msg);
+    }
+
+    // made protected for testing :-(
+    protected void sendMessage(Message msg) throws MessagingException {
+        Transport.send(msg);
+    }
+}
