625b960a82f40580b93588a33eeed044758b0aa7
Paulo Clavijo Esteban <paucls@gmail.com>
2021-06-08 22:01:12 +0100

    20210608-java-birthday-greetings - starting point

---
+++ 20210608-java-birthday-greetings/src/main/java/birthdaygreetings/adapters/out/JavaxEmailServer.java
@@ -0,0 +1,8 @@
+package birthdaygreetings;
+
+public class JavaxEmailServer implements EmailSender {
+    @Override
+    public void sendMessage(String subject, String body, String recipient) {
+
+    }
+}
