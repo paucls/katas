d5b524b95ad9c6a3939d602c1e730c621b2a2bc0
Paulo Clavijo Esteban <paucls@gmail.com>
2021-06-14 21:34:32 +0100

    20210608-java-birthday-greetings - refactor moving read employees to separated class

--- 20210608-java-birthday-greetings/src/main/java/birthdaygreetings/BirthdayService.java
+++ 20210608-java-birthday-greetings/src/main/java/birthdaygreetings/BirthdayService.java
@@ -1,11 +1,8 @@
 package birthdaygreetings;
 
 import javax.mail.MessagingException;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BirthdayService {
@@ -19,29 +16,14 @@ public class BirthdayService {
     public void sendGreetings(String fileName, OurDate ourDate) throws IOException, ParseException,
             MessagingException {
 
-        List<Employee> employees = readEmployees(fileName);
+        EmployeesReader employeesReader = new EmployeesReader();
+        List<Employee> employees = employeesReader.readEmployees(fileName);
 
         for (Employee employee : employees) {
             if (employee.isBirthday(ourDate)) {
                 sendGreetingTo(employee);
             }
         }
-    }
-
-    private List<Employee> readEmployees(String fileName) throws IOException, ParseException {
-        List<Employee> employees = new ArrayList<>();
-
-        BufferedReader in = new BufferedReader(new FileReader(fileName));
-        String str = "";
-        str = in.readLine(); // skip header
-        while ((str = in.readLine()) != null) {
-            String[] employeeData = str.split(", ");
-            Employee employee = new Employee(employeeData[1], employeeData[0],
-                    employeeData[2], employeeData[3]);
-            employees.add(employee);
-        }
-
-        return employees;
     }
 
     private void sendGreetingTo(Employee employee) throws MessagingException {
