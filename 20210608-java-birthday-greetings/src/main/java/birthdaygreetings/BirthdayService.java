52b2ec79970884fd13332b73449c6b7d358fed01
Paulo Clavijo Esteban <paucls@gmail.com>
2021-06-14 21:29:28 +0100

    20210608-java-birthday-greetings - refactor split phase

--- 20210608-java-birthday-greetings/src/main/java/birthdaygreetings/BirthdayService.java
+++ 20210608-java-birthday-greetings/src/main/java/birthdaygreetings/BirthdayService.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BirthdayService {
 
@@ -17,6 +19,8 @@ public class BirthdayService {
     public void sendGreetings(String fileName, OurDate ourDate) throws IOException, ParseException,
             MessagingException {
 
+        List<Employee> employees = new ArrayList<>();
+
         BufferedReader in = new BufferedReader(new FileReader(fileName));
         String str = "";
         str = in.readLine(); // skip header
@@ -24,7 +28,10 @@ public class BirthdayService {
             String[] employeeData = str.split(", ");
             Employee employee = new Employee(employeeData[1], employeeData[0],
                     employeeData[2], employeeData[3]);
+            employees.add(employee);
+        }
 
+        for (Employee employee : employees) {
             if (employee.isBirthday(ourDate)) {
                 sendEmailTo(employee);
             }
