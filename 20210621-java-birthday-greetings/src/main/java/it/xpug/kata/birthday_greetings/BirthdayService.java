872ff1b17b6f584a317c7488777a819c155bd573
Paulo Clavijo Esteban <paucls@gmail.com>
2021-06-22 07:36:50 +0100

    20210621-java-birthday-greetings - refactor split phase

--- 20210621-java-birthday-greetings/src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
+++ 20210621-java-birthday-greetings/src/main/java/it/xpug/kata/birthday_greetings/BirthdayService.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -16,12 +18,18 @@ import javax.mail.internet.MimeMessage;
 public class BirthdayService {
 
 	public void sendGreetings(String fileName, XDate xDate, String smtpHost, int smtpPort) throws IOException, ParseException, MessagingException {
+		List<Employee> employees = new ArrayList<>();
+
 		BufferedReader in = new BufferedReader(new FileReader(fileName));
 		String str = "";
 		str = in.readLine(); // skip header
 		while ((str = in.readLine()) != null) {
 			String[] employeeData = str.split(", ");
 			Employee employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
+			employees.add(employee);
+		}
+
+		for (Employee employee : employees) {
 			if (employee.isBirthday(xDate)) {
 				sendGreetingToEmployee(smtpHost, smtpPort, employee);
 			}
