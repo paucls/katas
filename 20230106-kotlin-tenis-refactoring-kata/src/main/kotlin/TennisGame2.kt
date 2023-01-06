140013037390c82a2488260c205be479589cb2a3
Paulo Clavijo Esteban <paulo.esteban@dell.com>
2023-01-06 13:40:43 +0000

    20230106-kotlin-tenis-refactoring-kata - game2 - rename field

--- 20230106-kotlin-tenis-refactoring-kata/src/main/kotlin/TennisGame2.kt
+++ 20230106-kotlin-tenis-refactoring-kata/src/main/kotlin/TennisGame2.kt
@@ -1,82 +1,82 @@
 class TennisGame2(private val player1Name: String, private val player2Name: String) : TennisGame {
-    var P1point: Int = 0
-    var P2point: Int = 0
+    private var player1Points: Int = 0
+    private var player2Points: Int = 0
 
     var P1res: String = ""
     var P2res: String = ""
 
     override fun getScore(): String {
         var score = ""
-        if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
+        if (player1Points == player2Points && player1Points < 4) {
+            if (player1Points == 0)
                 score = "Love"
-            if (P1point == 1)
+            if (player1Points == 1)
                 score = "Fifteen"
-            if (P1point == 2)
+            if (player1Points == 2)
                 score = "Thirty"
             score += "-All"
         }
-        if (P1point == P2point && P1point >= 3)
+        if (player1Points == player2Points && player1Points >= 3)
             score = "Deuce"
 
-        if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
+        if (player1Points > 0 && player2Points == 0) {
+            if (player1Points == 1)
                 P1res = "Fifteen"
-            if (P1point == 2)
+            if (player1Points == 2)
                 P1res = "Thirty"
-            if (P1point == 3)
+            if (player1Points == 3)
                 P1res = "Forty"
 
             P2res = "Love"
             score = "$P1res-$P2res"
         }
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
+        if (player2Points > 0 && player1Points == 0) {
+            if (player2Points == 1)
                 P2res = "Fifteen"
-            if (P2point == 2)
+            if (player2Points == 2)
                 P2res = "Thirty"
-            if (P2point == 3)
+            if (player2Points == 3)
                 P2res = "Forty"
 
             P1res = "Love"
             score = "$P1res-$P2res"
         }
 
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
+        if (player1Points > player2Points && player1Points < 4) {
+            if (player1Points == 2)
                 P1res = "Thirty"
-            if (P1point == 3)
+            if (player1Points == 3)
                 P1res = "Forty"
-            if (P2point == 1)
+            if (player2Points == 1)
                 P2res = "Fifteen"
-            if (P2point == 2)
+            if (player2Points == 2)
                 P2res = "Thirty"
             score = "$P1res-$P2res"
         }
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
+        if (player2Points > player1Points && player2Points < 4) {
+            if (player2Points == 2)
                 P2res = "Thirty"
-            if (P2point == 3)
+            if (player2Points == 3)
                 P2res = "Forty"
-            if (P1point == 1)
+            if (player1Points == 1)
                 P1res = "Fifteen"
-            if (P1point == 2)
+            if (player1Points == 2)
                 P1res = "Thirty"
             score = "$P1res-$P2res"
         }
 
-        if (P1point > P2point && P2point >= 3) {
+        if (player1Points > player2Points && player2Points >= 3) {
             score = "Advantage player1"
         }
 
-        if (P2point > P1point && P1point >= 3) {
+        if (player2Points > player1Points && player1Points >= 3) {
             score = "Advantage player2"
         }
 
-        if (P1point >= 4 && P2point >= 0 && P1point - P2point >= 2) {
+        if (player1Points >= 4 && player2Points >= 0 && player1Points - player2Points >= 2) {
             score = "Win for player1"
         }
-        if (P2point >= 4 && P1point >= 0 && P2point - P1point >= 2) {
+        if (player2Points >= 4 && player1Points >= 0 && player2Points - player1Points >= 2) {
             score = "Win for player2"
         }
         return score
@@ -84,9 +84,9 @@ class TennisGame2(private val player1Name: String, private val player2Name: Stri
 
     override fun wonPoint(player: String) {
         if (player === player1Name) {
-            P1point++
+            player1Points++
         } else {
-            P2point++
+            player2Points++
         }
     }
 }
