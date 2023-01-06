787f12e5a1c8c9371e7fc787828e7978c759a84e
Paulo Clavijo Esteban <paulo.esteban@dell.com>
2023-01-06 10:20:47 +0000

    20230106-kotlin-tenis-refactoring-kata - refactor name score var

--- 20230106-kotlin-tenis-refactoring-kata/src/main/kotlin/TennisGame1.kt
+++ 20230106-kotlin-tenis-refactoring-kata/src/main/kotlin/TennisGame1.kt
@@ -1,27 +1,30 @@
-class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
+class TennisGame1(
+    private val player1Name: String,
+    private val player2Name: String
+) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var player1Score: Int = 0
+    private var player2Score: Int = 0
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
-            m_score1 += 1
+            player1Score += 1
         else
-            m_score2 += 1
+            player2Score += 1
     }
 
     override fun getScore(): String {
         var score = ""
         var tempScore = 0
-        if (m_score1 == m_score2) {
-            when (m_score1) {
+        if (player1Score == player2Score) {
+            when (player1Score) {
                 0 -> score = "Love-All"
                 1 -> score = "Fifteen-All"
                 2 -> score = "Thirty-All"
                 else -> score = "Deuce"
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            val minusResult = player1Score - player2Score
             if (minusResult == 1)
                 score = "Advantage player1"
             else if (minusResult == -1)
@@ -33,10 +36,10 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         } else {
             for (i in 1..2) {
                 if (i == 1)
-                    tempScore = m_score1
+                    tempScore = player1Score
                 else {
                     score += "-"
-                    tempScore = m_score2
+                    tempScore = player2Score
                 }
                 when (tempScore) {
                     0 -> score += "Love"
