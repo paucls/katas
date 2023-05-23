89d3e67b32de3613eaf40531628e071474dfffe6
paulo <paulo@paulos-MacBook-Air.local>
2023-05-23 21:30:38 +0100

    20230503-java-gossiping - prep refactor growing a design that models Driver

--- 20230503-java-gossiping/src/main/java/Gossiping.java
+++ 20230503-java-gossiping/src/main/java/Gossiping.java
@@ -1,15 +1,48 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
 public class Gossiping {
     private final int[][] routes;
 
+    private final HashMap<Integer, Driver> drivers = new HashMap<>();
+
     public Gossiping(int[][] routes) {
         this.routes = routes;
+
+        IntStream.range(0, routes.length)
+                .forEach(i -> drivers.put(i, new Driver(i)));
     }
 
     public String howLongItTakes() {
+
         if (routes[0][0] == routes[1][0]) {
-            return "1";
+            drivers.get(0).indicateItKnowsAboutGossip(drivers.get(1).getGossipsItKnowsAbout());
+            drivers.get(1).indicateItKnowsAboutGossip(drivers.get(0).getGossipsItKnowsAbout());
+
+            if (drivers.get(0).getGossipsItKnowsAbout().size() == drivers.size()
+            && drivers.get(1).getGossipsItKnowsAbout().size() == drivers.size()) {
+                return "1";
+            }
         }
 
         return "never";
+    }
+}
+
+class Driver {
+    private final Set<Integer> gossipsItKnowsAbout = new HashSet<>();
+
+    public Driver(Integer driverNumber) {
+        gossipsItKnowsAbout.add(driverNumber);
+    }
+
+    public Set<Integer> getGossipsItKnowsAbout() {
+        return gossipsItKnowsAbout;
+    }
+
+    public void indicateItKnowsAboutGossip(Set<Integer> gossipNumber) {
+        gossipsItKnowsAbout.addAll(gossipNumber);
     }
 }
