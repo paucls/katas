1c510de807107e7b4e523be7bdb078d1fc032d07
Paulo Clavijo Esteban <paucls@gmail.com>
2024-07-29 13:47:10 +0100

    20240729-ts-fibonacci - refactor to parameterized tests

--- 20240729-ts-fibonacci/test/fibonacci.spec.ts
+++ 20240729-ts-fibonacci/test/fibonacci.spec.ts
@@ -3,23 +3,22 @@ import {Fibonacci} from "@/fibonacci";
 describe('Fibonacci Sequence', () => {
   const fibonacci = new Fibonacci();
 
-  it('should have 0 as its first number', () => {
-    expect(fibonacci.numberAt(0)).toBe(0);
+  describe('first two numbers should be same as position', () => {
+    test.each([
+      [0, 0],
+      [1, 1]
+    ])('%d, %d', (position, expected) => {
+      expect(fibonacci.numberAt(position)).toBe(expected);
+    });
   });
 
-  it('should have 1 as its second number', () => {
-    expect(fibonacci.numberAt(1)).toBe(1);
-  });
-
-  it('should have 1 as its third number', () => {
-    expect(fibonacci.numberAt(2)).toBe(1);
-  });
-
-  it('should have 2 as its fourth number', () => {
-    expect(fibonacci.numberAt(3)).toBe(2);
-  });
-
-  it('should have 5 as its sixth number', () => {
-    expect(fibonacci.numberAt(5)).toBe(5);
+  describe('other numbers are the sum of the two preceding ones', () => {
+    test.each([
+      [2, 1],
+      [3, 2],
+      [5, 5]
+    ])('%d, %d', (position, expected) => {
+      expect(fibonacci.numberAt(position)).toBe(expected);
+    });
   });
 });
