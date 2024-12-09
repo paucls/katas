3a4f738b9ea20b0d958a683febca32a5ed2a8b05
Paulo Clavijo Esteban <paucls@gmail.com>
2024-12-09 14:07:16 +0000

    20241209-java-gilded-rose - remove nesting levels

--- 20241209-java-gilded-rose/src/main/java/com/gildedrose/GildedRose.java
+++ 20241209-java-gilded-rose/src/main/java/com/gildedrose/GildedRose.java
@@ -42,25 +42,25 @@ class GildedRose {
             }
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            // do nothing
+        } else {
             item.sellIn = item.sellIn - 1;
         }
 
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+        if (item.name.equals("Aged Brie")) {
+            if (item.sellIn < 0 && item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.sellIn < 0) {
+                item.quality = 0;
+            }
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            // do nothing
+        } else {
+            if (item.sellIn < 0 && item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
     }
