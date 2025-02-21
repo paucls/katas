4f43a87ac144bcbb95eefc9109bf7aaee182bc95
Paulo Clavijo Esteban <paucls@gmail.com>
2025-02-21 12:05:12 +0000

    20250221-java-OfficeCleaningRobot-Refactoring - refactor main

--- 20250221-java-OfficeCleaningRobot-Refactoring/src/main/java/codingdojo/officecleaner9/OfficeCleaner9.java
+++ 20250221-java-OfficeCleaningRobot-Refactoring/src/main/java/codingdojo/officecleaner9/OfficeCleaner9.java
@@ -10,30 +10,38 @@ public class OfficeCleaner9 {
         Scanner input = new Scanner(System.in);
         int noCommands = Integer.parseInt(input.nextLine());
         //from problem - number of commands n(0 <= n <= 10,000).
-        if (noCommands < 0 || noCommands > 10000 )
-        { return; }
-        //read starting location of robot
-        String[] startPoints = input.nextLine().split(" "); ;
-        var robot = new RobotCleaner();
-        //Visits the starting location
-        robot.StartAt(Integer.parseInt(startPoints[0]), Integer.parseInt(startPoints[1]));
-
-        List<String> listOfCommands = new ArrayList<>();
-        for (int i = 0; i < noCommands; i++)
-        {
-            listOfCommands.add(input.nextLine());
+        if (noCommands < 0 || noCommands > 10000) {
+            return;
         }
 
-        //Reads the direction and number of steps and Visits the floor in each direction
-        for (String command : listOfCommands)
-        {
-            String[] options = command.split(" ");
+        // PARSING
+        //read starting location of robot
+        String[] startPoints = input.nextLine().split(" ");
+        int x = Integer.parseInt(startPoints[0]);
+        int y = Integer.parseInt(startPoints[1]);
 
+        List<String[]> commandOptions = readCommands(noCommands, input);
+
+        // EXECUTE ROBOT
+        var robot = new RobotCleaner();
+
+        robot.StartAt(x, y);
+
+        //Reads the direction and number of steps and Visits the floor in each direction
+        for (String[] options : commandOptions) {
             robot.CleanFloor(options[0].charAt(0), Integer.parseInt(options[1]));
         }
 
         robot.PrintVisitedPlaces();
         System.out.println(String.format("=> Cleaned: %s", robot.visitedPlaces.size()));
+    }
 
+    private static List<String[]> readCommands(int noCommands, Scanner input) {
+        List<String[]> commandOptions = new ArrayList<>();
+        for (int i = 0; i < noCommands; i++) {
+            String[] options = input.nextLine().split(" ");
+            commandOptions.add(options);
+        }
+        return commandOptions;
     }
 }
